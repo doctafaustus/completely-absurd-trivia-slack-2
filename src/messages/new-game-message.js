d8ced42c43a6dbf6e4a8c8a5ba4c8d93052ffa53
doctafaustus <clowd150@gmail.com>
2019-09-24 00:49:29 -0500

    Test /new-game hook

--- src/messages/new-game-message.js
+++ src/messages/new-game-message.js
@@ -2,7 +2,7 @@ module.exports = function newGameMessage() {
   return {
     'attachments': [
       {
-        'text': `A new game has been created! :speak_no_evil: \nWho would like to play?`,
+        'text': `A new game has been created! :speak_no_evil: \n*Who would like to play?*`,
         'callback_id': 'join_game',
         'color': '#03ec42',
         'attachment_type': 'default',
