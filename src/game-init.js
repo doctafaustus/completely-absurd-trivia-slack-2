1f6a57d5b59b9c5363d2f5692beb8ff2af4cfe07
doctafaustus <clowd150@gmail.com>
2019-09-24 00:44:29 -0500

    Test /new-game hook

--- src/game-init.js
+++ src/game-init.js
@@ -13,14 +13,30 @@ module.exports = function gameInit(app) {
   let game;
   const user = new User('Bill');
 
-  console.log('urlencodedParser', app.urlencodedParser);
-
   app.post('/new-game', app.urlencodedParser, (req, res) => {
     console.log('/new-game');
     res.status(200).end();
   
     game = new Game();
-    sendMessageToSlack(app.webhookURL, newGameMessage);
+    sendMessageToSlack(app.webhookURL, {
+    'attachments': [
+      {
+        'text': `A new game has been created! \nWho would like to play?`,
+        'callback_id': 'join_game',
+        'color': '#2ea664',
+        'attachment_type': 'default',
+        'actions': [
+          {
+            'name': 'me',
+            'text': 'Me!',
+            'type': 'button',
+            'value': 'me',
+            'style': 'primary'
+          }
+        ]
+      }
+    ]
+  });
   });
 
 }
