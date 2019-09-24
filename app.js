b990033d9f7a9c54cd5281088e0a7b39373f0028
doctafaustus <clowd150@gmail.com>
2019-09-24 00:33:21 -0500

    Test /new-game hook

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Game initializaiton
 const gameInit = require('./src/game-init.js');
@@ -34,33 +34,7 @@ const prodSecretURL = process.env.PORT ? process.env.prodSecretURL : fs.readFile
 const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodSecretURL;
 
-// gameInit(app);
+gameInit(app);
 
 
-
-app.get('/new-game', urlencodedParser, (req, res) => {
-  console.log('/new-game');
-  // res.status(200).end();
-
-  // game = new Game();
-  // sendMessageToSlack(app.webhookURL, {
-  //   'attachments': [
-  //     {
-  //       'text': `A new game has been created! \nWho would like to play?`,
-  //       'callback_id': 'join_game',
-  //       'color': '#2ea664',
-  //       'attachment_type': 'default',
-  //       'actions': [
-  //         {
-  //           'name': 'me',
-  //           'text': 'Me!',
-  //           'type': 'button',
-  //           'value': 'me',
-  //           'style': 'primary'
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // });
-});
 
