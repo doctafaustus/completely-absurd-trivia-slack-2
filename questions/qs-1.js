9e428fb3a1efa11639222877674e678b92f323c8
doctafaustus <clowd150@gmail.com>
2019-10-10 00:26:58 -0500

    Add qs-1

--- questions/qs-1.js
+++ questions/qs-1.js
@@ -2,84 +2,84 @@ module.exports = {
   gameType: 'text',
   questions: [
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'Where in the body is most of the element iron located?', 
+      a: 'Bones', 
+      b: 'Blood',
+      c: 'Skin',
+      d: 'Brain',
+      answer: 'b'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'Where was former President Obama born?', 
+      a: 'Illinois', 
+      b: 'Michigan',
+      c: 'Ohio',
+      d: 'Hawaii',
+      answer: 'd'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'The flag of Jamaica includes the colors black, green, and _____.', 
+      a: 'White', 
+      b: 'Orange',
+      c: 'Red',
+      d: 'Yellow',
+      answer: 'd'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'Also known as a bomber hat, which hat is a leather cap with large earflaps, a chin strap, and often has a fleece lining?', 
+      a: 'Aviator Hat', 
+      b: 'Panama Hat',
+      c: 'Deerstalker Hat',
+      d: 'Boonie Hat',
+      answer: 'a'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'What Swiss beverage company started in 1783 and sells lemonade, carbonated waters, and ginger ales?', 
+      a: 'Stewarts', 
+      b: 'Fanta',
+      c: 'Schweppes',
+      d: 'Vernors',
+      answer: 'c'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'Who voiced Mr. Fox in the 2009 movie "Fantastic Mr. Fox"?', 
+      a: 'George Clooney', 
+      b: 'Brad Pitt',
+      c: 'Adam Sandler',
+      d: 'Benedict Cumberbatch',
+      answer: 'a'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'What TV show began in 1987 and consisted of heavy metal music videos airing on MTV?', 
+      a: 'Headbangers Ball', 
+      b: 'V-Rock',
+      c: 'Heavy Metal Mayhem',
+      d: 'Super Metal World',
+      answer: 'a'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'What late night TV host gained notoriety for previously hosting "The Man Show" and "Win Ben Stein\'s Money"?', 
+      a: 'Craig Ferguson', 
+      b: 'Conan O\'Brien',
+      c: 'Jimmy Kimmel',
+      d: 'Seth Myers',
+      answer: 'c'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'What do you call an atom that has more protons than electrons?', 
+      a: 'Molecule', 
+      b: 'Isotope',
+      c: 'Anion',
+      d: 'Cation',
+      answer: 'd'
     },
     {
-      question: '', 
-      a: '', 
-      b: '',
-      c: '',
-      d: '',
-      answer: ''
+      question: 'Which sport is played using rackets to hit a shuttlecock across a net?', 
+      a: 'Racquetball', 
+      b: 'Badminton',
+      c: 'Squash',
+      d: 'Aikido',
+      answer: 'b'
     }
   ]
 };
