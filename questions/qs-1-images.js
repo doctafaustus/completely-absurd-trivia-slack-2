ced2109528902d4e6a9bdd9b80a5200032979eea
doctafaustus <clowd150@gmail.com>
2019-10-10 18:50:15 -0500

    Set ready for 2.0 production launch

---
+++ questions/qs-1-images.js
@@ -0,0 +1,95 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'Who is this?',
+      imageURL: 'https://www.intheblack.com/-/media/intheblack/allimages/magazine-2018/06-june/satya-nadella-microsoft-ceo.jpg', 
+      a: 'Shantanu Narayen',
+      b: 'Satya Nadella',
+      c: 'Philippe Geubels',
+      d: 'Dara Khosrowshahi',
+      answer: 'b'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2018/04/yy.png', 
+      a: 'Dorothy',
+      b: 'Liz',
+      c: 'Penny',
+      d: 'Fiona',
+      answer: 'c'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://i.ibb.co/RP2wp4Y/Janet-Reno-us-Portrait.jpg', 
+      a: 'Dianne Feinstein',
+      b: 'Jo Cox',
+      c: 'Margaret Thatcher',
+      d: 'Janet Reno',
+      answer: 'd'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://i.ibb.co/rs2nc79/Bonkers3.png', 
+      a: 'Bonkers',
+      b: 'Heathcliff',
+      c: 'Felix',
+      d: 'Top Cat',
+      answer: 'a'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Andy_Warhol_by_Jack_Mitchell.jpg', 
+      a: 'Nigel Tufnel',
+      b: 'Andy Warhol',
+      c: 'Roy Lichtenstein',
+      d: 'David Hockney',
+      answer: 'b'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'http://37.media.tumblr.com/6b329347244c9bd173a2bda690f2dabd/tumblr_mrw3j50UQa1szbfero1_500.jpg', 
+      a: 'Aaliyah',
+      b: 'Mya',
+      c: 'Toni Braxton',
+      d: 'Eve',
+      answer: 'a'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://img.huffingtonpost.com/asset/5bb5c52e2400005100567ed0.jpeg', 
+      a: 'Lestor Holt',
+      b: 'Brian Williams',
+      c: 'Dan Rather',
+      d: 'Tom Brokaw',
+      answer: 'b'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://beckett-www.s3.amazonaws.com/news/news-content/uploads/2018/01/Daredevil-feature.jpg', 
+      a: 'The Flash',
+      b: 'Deadpool',
+      c: 'Ant-Man',
+      d: 'Daredevil',
+      answer: 'd'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'http://stoneyroads.com/wp-content/uploads/2017/02/daftpunk-1200x800.jpg', 
+      a: 'Stardust',
+      b: 'Daft Punk',
+      c: 'deadmau5',
+      d: 'Cybertronic',
+      answer: 'b'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://cdn.theatlantic.com/assets/media/img/mt/2016/12/00752a-1/lead_720_405.jpg', 
+      a: 'Mark Twain',
+      b: 'Charles Dickens',
+      c: 'Charles Darwin',
+      d: 'Walt Whitman',
+      answer: 'd'
+    },
+  ]
+};
---
+++ questions/template-images.js
@@ -0,0 +1,95 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '',
+      imageURL: '', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+  ]
+};
