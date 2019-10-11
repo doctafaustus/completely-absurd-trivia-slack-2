51995d55715ce1ae2ccb73499e7cb3a68b31c6e2
doctafaustus <clowd150@gmail.com>
2019-10-11 14:13:45 -0500

    Config first image game

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-images.js');
+app.qs = require('./questions/qs-1-images.js');
 
 gameInit(app);
 
