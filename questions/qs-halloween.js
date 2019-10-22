ec3dd463dcda5fb0cce605ed9ebae25512a2f3a5
doctafaustus <clowd150@gmail.com>
2019-10-21 22:30:12 -0500

    Add a few Halloween questions

---
+++ questions/qs-halloween.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'Where does the annual "Village Halloween Parade" take place?', 
+      a: 'Boise ID', 
+      b: 'Sleepy Hollow NY',
+      c: 'Chicago IL',
+      d: 'New York City',
+      answer: ''
+    },
+    {
+      question: 'Bram Stoker was famous for writing which novel?', 
+      a: 'Carmilla', 
+      b: 'Jekyll and Hyde',
+      c: 'Frankenstein',
+      d: 'Dracula',
+      answer: 'd'
+    },
+    {
+      question: 'What is typically the center color in candy corn?', 
+      a: 'White', 
+      b: 'Yellow',
+      c: 'Orange',
+      d: 'Red',
+      answer: 'c'
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    }
+  ]
+};
