c181face47dd403de53cf92d3dba153193c08ba7
doctafaustus <clowd150@gmail.com>
2019-10-24 22:36:04 -0500

    Add qs images

---
+++ questions/qs-halloween-images.js
@@ -0,0 +1,95 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'Name this movie.',
+      imageURL: 'https://i.ibb.co/SNmCXc3/1.jpg', 
+      a: 'Maid of Salem',
+      b: 'Halloweentown',
+      c: 'Hocus Pocus',
+      d: 'The Witches',
+      answer: 'd'
+    },
+    {
+      question: 'Name this character.',
+      imageURL: 'https://i.ibb.co/vHX57jh/fester.jpg', 
+      a: 'Herman Munster',
+      b: 'Grandpa Munster',
+      c: 'Uncle Fester',
+      d: 'Lurch',
+      answer: 'c'
+    },
+    {
+      question: 'What type of pumpkin is this?',
+      imageURL: 'https://i.ibb.co/28TrH3j/pumpkin.jpg', 
+      a: 'Baby Bear',
+      b: 'Sweet Dumpling',
+      c: 'Carnival',
+      d: 'Cinderella',
+      answer: 'd'
+    },
+    {
+      question: 'In what town is this house located?',
+      imageURL: 'https://i.ibb.co/Drg5PV1/amittyville.jpg', 
+      a: 'Amityville',
+      b: 'Winchester',
+      c: 'Fall River',
+      d: 'Salem',
+      answer: 'a'
+    },
+    {
+      question: 'Who authored the book with this cover?',
+      imageURL: 'https://i.ibb.co/1vwHnb3/goosebumps.jpg', 
+      a: 'Clive Barker',
+      b: 'Alvin Schwartz',
+      c: 'RL Stine',
+      d: 'Stephen King',
+      answer: 'c'
+    },
+    {
+      question: 'Who painted this painting?',
+      imageURL: 'https://i.ibb.co/thM9mPX/the-scream.jpg', 
+      a: 'Edvard Munch',
+      b: 'Vincent van Gogh',
+      c: 'Salvador Dali',
+      d: 'Jackson Pollock',
+      answer: 'a'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://i.ibb.co/ZYMRKVH/price.jpg', 
+      a: 'Edward Price',
+      b: 'Edgar Allan Poe',
+      c: 'Lon Chaney',
+      d: 'Vincent Price',
+      answer: 'd'
+    },
+    {
+      question: 'This band guest starred in this Halloween episode of South Park.',
+      imageURL: 'https://i.ibb.co/59QBv72/korn.jpg', 
+      a: 'Radiohead',
+      b: 'Foo Fighers',
+      c: 'Korn',
+      d: 'Metallica',
+      answer: 'c'
+    },
+    {
+      question: 'This celebrity often dresses up for Halloween on the red carpet.',
+      imageURL: 'https://i.ibb.co/3vz31jv/klum.jpg', 
+      a: 'Jennifer Lawrence',
+      b: 'Heidi Klum',
+      c: 'Kate Moss',
+      d: 'Gwyneth Paltrow',
+      answer: 'b'
+    },
+    {
+      question: 'What is the name of this candy?',
+      imageURL: 'https://i.ibb.co/W2FmS4P/necco.jpg', 
+      a: 'Necco',
+      b: 'Zabas',
+      c: 'Smartees',
+      d: 'Chuckles',
+      answer: 'a'
+    },
+  ]
+};
