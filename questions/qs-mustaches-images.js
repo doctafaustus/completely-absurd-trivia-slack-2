aa14799f49977900b5566698b6bfa32f9548ae0f
William Coloe <william.coloe@accenture.com>
2019-11-12 16:54:37 -0600

    Test mustache game

---
+++ questions/qs-mustaches-images.js
@@ -0,0 +1,95 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/SVJkzv5/mustache1.png', 
+      a: 'Robert Redford',
+      b: 'Ryan Gosling',
+      c: 'Brad Pitt',
+      d: 'Tom Brady',
+      answer: 'c'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/1XhQTnC/mustache2.png', 
+      a: 'Densel Washington',
+      b: 'Jamie Foxx',
+      c: 'Wayne Brady',
+      d: 'Will Smith',
+      answer: 'b'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/pnzcJ0Z/mustache3.png', 
+      a: 'Billie Dee Williams',
+      b: 'Linoel Richie',
+      c: 'Michael B Jordan',
+      d: 'Richard Pryor',
+      answer: 'a'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'ttps://i.ibb.co/QYBcjKg/mustache4-2.png', 
+      a: 'Danny Trejo',
+      b: 'Luis Guzman',
+      c: 'Ricardo Montalbán',
+      d: 'Edward James Olmos',
+      answer: 'd'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/zJ4fG9V/mustache5.png', 
+      a: 'Henry Cavill',
+      b: 'ames Marsden',
+      c: 'Jamie Dornan',
+      d: 'Matt Bomer',
+      answer: 'd'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/mBTR0d5/mustache6.png', 
+      a: 'Jared Leto',
+      b: 'Adam Levine',
+      c: 'Jonathan Van Ness',
+      d: 'Keanu Reeves',
+      answer: 'c'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/fYgHsS6/mustache7-1.png',
+      a: 'Colin Firth',
+      b: 'Jonathan Rhys Meyers',
+      c: 'Johnny Lee Miller',
+      d: 'Colin Ferrell',
+      answer: 'd'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/N6bqV9T/mustache8.png', 
+      a: 'Michael Fassbender',
+      b: 'Peter Sarsgarrd',
+      c: 'Alexander Skarsgard',
+      d: 'Orlando Bloom',
+      answer: 'c'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/fDHnpBD/mustache9.png', 
+      a: 'Henry Golding',
+      b: 'Randall Park',
+      c: 'Daniel Day Kim',
+      d: 'Steven Yeun',
+      answer: 'd'
+    },
+    {
+      question: 'Whose mustache is this?',
+      imageURL: 'https://i.ibb.co/wWxD7C6/mustache10.png', 
+      a: 'Alan Rickman',
+      b: 'Jeremy Irons',
+      c: 'Ian McKellen',
+      d: 'David Warner',
+      answer: 'a'
+    },
+  ]
+};
