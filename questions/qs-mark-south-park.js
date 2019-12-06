2a69bf69c43e42572f0d8bc554c20efd232c57c1
doctafaustus <clowd150@gmail.com>
2019-12-06 09:19:53 -0600

    Add qs mark south park

---
+++ questions/qs-mark-south-park.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'in the episode "go god go," the warring factions battle over the right name for their aetheist denomination. which is not one of the factions?', 
+      a: 'alligned atheist order (AAO)', 
+      b: 'unified atheist league (UAL)',
+      c: 'united atheist alliance (UAA)',
+      d: 'allied atheist allegiance (AAA)',
+      answer: 'a'
+    },
+    {
+      question: 'randy marshs marijuana farm is called', 
+      a: 'brevity', 
+      b: 'tegridy',
+      c: 'selenity',
+      d: 'tertiary',
+      answer: 'b'
+    },
+    {
+      question: 'cartmans father is', 
+      a: 'dr. mephesto', 
+      b: 'one of the 1989 denver boncos',
+      c: 'jack tenorman',
+      d: 'chef',
+      answer: 'c'
+    },
+    {
+      question: 'the music album "chef aid" did not feature a song from', 
+      a: 'master p', 
+      b: 'elton john',
+      c: 'devo',
+      d: 'phil collins',
+      answer: 'd'
+    },
+    {
+      question: 'cartmans favorite mexican restaurant is', 
+      a: 'casa bonita', 
+      b: 'la cortina',
+      c: 'sombreros',
+      d: 'three amigos',
+      answer: 'a'
+    },
+    {
+      question: 'what is the name of matt & treys band that occassionally makes music for south park?', 
+      a: 'pisswater faucets', 
+      b: 'DVDA',
+      c: 'bananas and blow',
+      d: 'pile driver',
+      answer: 'b'
+    },
+    {
+      question: 'what is the name of the abusive boy scout leader who replaces big gay al?', 
+      a: 'frank fondler', 
+      b: 'small straight larry',
+      c: 'mr slippyfist',
+      d: 'mr mackey',
+      answer: 'c'
+    },
+    {
+      question: 'the theme song of south park is performed by', 
+      a: 'mr bungle', 
+      b: 'big head todd and the monsters',
+      c: 'ween',
+      d: 'primus',
+      answer: 'd'
+    },
+    {
+      question: 'the alter ego of butters is', 
+      a: 'professor chaos', 
+      b: 'general dissaray',
+      c: 'the coon',
+      d: 'mintberry crunch',
+      answer: 'a'
+    },
+    {
+      question: 'in a halloween episode, spooky vision refers to', 
+      a: 'goggles that make ghosts appear', 
+      b: 'barbara streissand\'s face in the corners of the screen',
+      c: 'paris hilton under a black light',
+      d: 'being hipnotized by lasers',
+      answer: 'b'
+    }
+  ]
+};
