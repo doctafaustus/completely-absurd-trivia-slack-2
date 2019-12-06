1b799590532b2e8ad11461be60da956293c0824c
William Coloe <william.coloe@accenture.com>
2019-12-06 13:35:20 -0600

    Config qs mark

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.webhookURL = prodRealURL;
 
 // Set questions
 //qs-james-thanksgiving-images.js
-app.qs = require('./questions/qs-james-2.js');
+app.qs = require('./questions/qs-mark-text.js');
 
 gameInit(app);
