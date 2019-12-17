7786ec12819b280032e1b3cca76535c29b8544ed
doctafaustus <clowd150@gmail.com>
2019-12-17 16:53:50 -0600

    Begin qs-3

---
+++ questions/qs-3.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'Who played Charles in th TV sitcom "Charles in Charge?"', 
+      a: 'John Stamos', 
+      b: 'Bronson Pinchot',
+      c: 'Robin Williams',
+      d: 'Scott Daio',
+      answer: 'd'
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    }
+  ]
+};
