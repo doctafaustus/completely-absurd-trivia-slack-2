511d13f57e3241d863594a95cae3ff0f231cf9d1
William Coloe <william.coloe@accenture.com>
2019-12-18 14:55:27 -0600

    Add star wars qs

---
+++ questions/qs-marion-star-wars.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'In the latest version of A New Hope on Disney+, what does Greedo say before being shot by Han in Mos Eisley?', 
+      a: 'Chumbawamba!', 
+      b: 'Utinni!',
+      c: 'McClunky!',
+      d: 'Oonta Goota, Solo!',
+      answer: 'c'
+    },
+    {
+      question: 'How many languages is C-3PO fluent?', 
+      a: 'More than 6 million', 
+      b: 'More than 5 million',
+      c: 'More than 6,000',
+      d: 'More than 200',
+      answer: 'a'
+    },
+    {
+      question: 'Who sings the song "Happy Life Day" in the infamous Star Wars Holiday Special of 1978?', 
+      a: 'Princess Leia', 
+      b: 'Luke Skywalker',
+      c: 'Yoda',
+      d: 'C-3P0',
+      answer: 'a'
+    },
+    {
+      question: 'Who played Queen Amidala\'s body double in Phantom Menace?', 
+      a: 'Keisha Castle-Hughes', 
+      b: 'Keira Knightley',
+      c: 'Tina Majorino ',
+      d: 'Christina Ricci',
+      answer: 'b'
+    },
+    {
+      question: 'Which Star Wars movies did George Lucas direct?', 
+      a: 'Episodes 1-6', 
+      b: 'Episodes 1-3',
+      c: 'Episodes 1-4',
+      d: 'Episodes 2-5',
+      answer: 'c'
+    },
+    {
+      question: 'Who plays lil Anakin Skywalker in episode 1?', 
+      a: 'Nicholas Hoult', 
+      b: 'Haley Joel Osment',
+      c: 'Jonathan Lipnicki',
+      d: 'Jake Lloyd',
+      answer: 'd'
+    },
+    {
+      question: 'Who apparently turned down the opportunity to direct Return of the Jedi (according to their autobiography)?', 
+      a: 'John Hughes', 
+      b: 'James Cameron',
+      c: 'Martin Scorsese',
+      d: 'David Lynch',
+      answer: 'd'
+    },
+    {
+      question: 'What is Laura Dern\'s character name in Last Jedi?', 
+      a: 'General Sweitt Concorkill', 
+      b: 'Senior Commander Ello Asty',
+      c: 'Vice Admiral Amilyn Holdo',
+      d: 'Lieutenant Milton Putna',
+      answer: 'c'
+    },
+    {
+      question: 'Which of these is a real character in Star Wars?', 
+      a: 'Myron Wagtail', 
+      b: 'Salacious B. Crumb',
+      c: 'Worf',
+      d: 'Barliman Butterbur',
+      answer: 'b'
+    },
+    {
+      question: 'Which Star Wars movie has had the most successful box office opening weekend performance?', 
+      a: 'Phantom Menace', 
+      b: 'Revenge of the Sith',
+      c: 'Force Awakens',
+      d: 'The Last Jedi',
+      answer: 'c'
+    }
+  ]
+};
