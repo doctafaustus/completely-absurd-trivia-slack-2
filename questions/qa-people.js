b5b715faf9cc38f6bbd201f2e888f3e8ab5b8051
William Coloe <william.coloe@accenture.com>
2019-12-20 14:08:07 -0600

    Add people trivia

---
+++ questions/qa-people.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'What job has Jane NOT held?', 
+      a: 'Convenience Store', 
+      b: 'Egg Gaherer',
+      c: 'Car Mechanic',
+      d: 'A Company President',
+      answer: 'c'
+    },
+    {
+      question: 'Which famous marriage did Taylor\'s cousin break up?', 
+      a: 'Puff Daddy & JLo', 
+      b: 'Carson Daly & Tara Reid',
+      c: 'Ashton Kutcher & Demi Moore',
+      d: 'Jared Leto & Cameron Diaz',
+      answer: 'c'
+    },
+    {
+      question: 'What TV show did Tom Hanlon\'s former employer star in?', 
+      a: 'America\'s Most Wanted', 
+      b: 'Next',
+      c: 'Room Raiders',
+      d: 'Jerry Springer',
+      answer: 'a'
+    },
+    {
+      question: 'Who did JLowe model golf stuff for?', 
+      a: 'Academy', 
+      b: 'Dick\'s Sporting Goods',
+      c: 'Sports Authority',
+      d: 'Canadian Tire',
+      answer: 'b'
+    },
+    {
+      question: 'Aaron\'s wife has written what type of book?', 
+      a: 'Fiction', 
+      b: 'Romance/Smut',
+      c: 'Adventure',
+      d: 'Young Adult',
+      answer: 'b'
+    },
+    {
+      question: 'Lynn has NOT broken this bone.', 
+      a: 'Nose', 
+      b: 'Humerus',
+      c: 'Ankle',
+      d: 'Tibula',
+      answer: 'd'
+    },
+    {
+      question: 'What was Troy\'s Middle School extracurricular activity?', 
+      a: 'Mariachi Band', 
+      b: 'Woodworking',
+      c: 'Ultimate Frisbee',
+      d: 'Debate',
+      answer: 'a'
+    },
+    {
+      question: 'What character did Catherine play in Middle School theater?', 
+      a: 'The Mad Hatter', 
+      b: 'The White Rabbit',
+      c: 'The Cheshire Cat',
+      d: 'The Caterpillar',
+      answer: 'b'
+    },
+    {
+      question: 'Who went to space camp at NASA?', 
+      a: 'Elizadeath', 
+      b: 'Joelle',
+      c: 'Nick',
+      d: 'Lauren',
+      answer: 'd'
+    },
+    {
+      question: 'Which of the following Clearheaders is 25% Korean?', 
+      a: 'Lauren', 
+      b: 'Taylor',
+      c: 'Tyler',
+      d: 'Colby',
+      answer: 'd'
+    }
+  ]
+};
