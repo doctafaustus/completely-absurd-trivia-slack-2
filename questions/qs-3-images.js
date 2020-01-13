afa8c97f87b343b7fdc2bc531abcdafb1537f46d
William Coloe <william.coloe@accenture.com>
2020-01-13 15:18:24 -0600

    Test qs-3-images

---
+++ questions/qs-3-images.js
@@ -0,0 +1,95 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'Who painted this?',
+      imageURL: 'https://i.ibb.co/c6ZDf43/painting.jpg', 
+      a: 'Jean-Honore Fragonard',
+      b: 'Jacques-Louis David',
+      c: 'Rembrandt',
+      d: 'Antoine Watteau',
+      answer: 'c'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://i.ibb.co/vsVxJMS/charlie-chaplin-9244327-2-402.jpg', 
+      a: 'Charlie Chaplin',
+      b: 'Elvis',
+      c: 'Gene Kelly',
+      d: 'Donald O\'Connor',
+      answer: 'a'
+    },
+    {
+      question: 'What state can these rocks be found in?',
+      imageURL: 'https://i.ibb.co/qJ2GJzP/Screen-Shot-2020-01-13-at-2-37-24-PM.png', 
+      a: 'Texas',
+      b: 'Utah',
+      c: 'Arizona',
+      d: 'New Mexico',
+      answer: 'd'
+    },
+    {
+      question: 'The "aardwolf" is a type of ____?',
+      imageURL: 'https://i.ibb.co/WpVKH04/Screen-Shot-2020-01-13-at-2-39-37-PM.png', 
+      a: 'Fox',
+      b: 'Hyena',
+      c: 'Wolf',
+      d: 'Weasel',
+      answer: 'b'
+    },
+    {
+      question: 'What sport is this?',
+      imageURL: 'https://i.ibb.co/hHV8kWS/Screen-Shot-2020-01-13-at-2-41-40-PM.png', 
+      a: 'Bocce Ball',
+      b: 'Cricket',
+      c: 'Rounders',
+      d: 'Croquet',
+      answer: 'd'
+    },
+    {
+      question: 'What logo is this?',
+      imageURL: 'https://i.ibb.co/zN59Yby/Screen-Shot-2020-01-13-at-2-45-02-PM.png', 
+      a: 'Zune',
+      b: 'Shutterstock',
+      c: 'Nova Launcher',
+      d: 'Heap Analytics',
+      answer: 'a'
+    },
+    {
+      question: 'What building is this?',
+      imageURL: 'https://i.ibb.co/m0XRQSX/Screen-Shot-2020-01-13-at-2-48-56-PM.png', 
+      a: 'Pantheon',
+      b: 'Basilica of Maxentius',
+      c: 'Temple of Saturn',
+      d: 'Parthenon',
+      answer: 'd'
+    },
+    {
+      question: 'What type of car part is this?',
+      imageURL: 'https://i.ibb.co/BrRXYKR/Screen-Shot-2020-01-13-at-2-54-36-PM.png', 
+      a: 'Muffler',
+      b: 'Carburetor',
+      c: 'Alternator',
+      d: 'Radiator',
+      answer: 'a'
+    },
+    {
+      question: 'Who wrote this book?',
+      imageURL: 'https://i.ibb.co/zsBxwVM/Screen-Shot-2020-01-13-at-2-58-16-PM.png', 
+      a: 'Eric Carle',
+      b: 'Shel Silverstein',
+      c: 'Maurice Sendak',
+      d: 'Dr. Suess',
+      answer: 'a'
+    },
+    {
+      question: 'This mascot belong to which city\'s baseball team?',
+      imageURL: 'https://i.ibb.co/wNMTVyx/Screen-Shot-2020-01-13-at-3-01-31-PM.png', 
+      a: 'Atlanta',
+      b: 'Minnesota',
+      c: 'Philadelphia',
+      d: 'Detroit',
+      answer: 'c'
+    },
+  ]
+};
