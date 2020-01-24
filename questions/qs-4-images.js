e5455352fb8520870d31dbcc12d53b79a4000904
doctafaustus <clowd150@gmail.com>
2020-01-24 11:29:36 -0600

    Add qs4 images

---
+++ questions/qs-4-images.js
@@ -0,0 +1,95 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'What instrument is this?',
+      imageURL: 'https://i.ibb.co/RS2ZNjq/lute.jpg', 
+      a: 'Lyre',
+      b: 'Guitar',
+      c: 'Sitar',
+      d: 'Lute',
+      answer: 'd'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://i.ibb.co/2YXZzfV/crissangel-1.jpg', 
+      a: 'Gerard Way',
+      b: 'Criss Angel',
+      c: 'Robert Pattinson',
+      d: 'Davey Havok',
+      answer: 'b'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/K71nppp/finland-1.png', 
+      a: 'Denmark',
+      b: 'Sweden',
+      c: 'Norway',
+      d: 'Finland',
+      answer: 'd'
+    },
+    {
+      question: 'What logo is this?',
+      imageURL: 'https://i.ibb.co/V992dQ0/top4g-XBl-400x400-1.png', 
+      a: 'TuneIn',
+      b: 'TaylorMade',
+      c: 'Tidal',
+      d: 'Twilio',
+      answer: 'b'
+    },
+    {
+      question: 'What cereal is this?',
+      imageURL: 'https://i.ibb.co/nnx5BwY/348s.jpg', 
+      a: 'Fruity Cherios',
+      b: 'Lucky Charms',
+      c: 'Apple Jacks',
+      d: 'Froot Loops',
+      answer: 'c'
+    },
+    {
+      question: 'What movie is this from?',
+      imageURL: 'https://i.ibb.co/bQ2PKpj/maxresdefault-1.jpg', 
+      a: 'Mary Poppins',
+      b: '101 Dalmations',
+      c: 'Dumbo',
+      d: 'Fantasia',
+      answer: 'a'
+    },
+    {
+      question: 'This is a closeup of what action figure?',
+      imageURL: 'https://i.ibb.co/PwJhMLP/heman.jpg', 
+      a: 'Lion-O',
+      b: 'He-Man',
+      c: 'Mumrah',
+      d: 'Hulk Hogan',
+      answer: 'b'
+    },
+    {
+      question: 'Which celebrity is sporting this tattoo?',
+      imageURL: 'https://i.ibb.co/R44DwBd/gaga-1.jpg', 
+      a: 'Lady Gaga',
+      b: 'Angelina Jolie',
+      c: 'Jennifer Lawrence',
+      d: 'Hilary Duff',
+      answer: 'a'
+    },
+    {
+      question: 'Who is this?',
+      imageURL: 'https://i.ibb.co/bWh5n1s/13-millard-fillmore-3-gallery-1.jpg', 
+      a: 'James Buchanan',
+      b: 'Oliver Cromwell',
+      c: 'Ralph Waldo Emerson',
+      d: 'Millard Fillmore',
+      answer: 'd'
+    },
+    {
+      question: 'What is this Muppet\'s name?',
+      imageURL: 'https://i.ibb.co/jLLF4Gj/Dr-Bunsen-Honeydew.jpg', 
+      a: 'Bunsen Honeydew',
+      b: 'Beaker Newton',
+      c: 'Waldorf Cosgrove',
+      d: 'Walter Newsman',
+      answer: 'a'
+    },
+  ]
+};
