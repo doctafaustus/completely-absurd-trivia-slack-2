814aed634cfae42a76bbd8f179502fe6efbfb5a6
William Coloe <william.coloe@accenture.com>
2020-02-03 10:14:20 -0600

    Add qs-ra-batman.js

---
+++ qs-ra-batman.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'What is the name of Batman\'s father?', 
+      a: 'Bruce Wayne Sr.', 
+      b: 'John Wayne Gacy',
+      c: 'Thomas Wayne',
+      d: 'James Wayne',
+      answer: 'c'
+    },
+    {
+      question: 'Batman had a son with the daughter of one of his biggest enemies. Which villain\'s daughter was it?', 
+      a: 'Ra\'s al Ghul', 
+      b: 'Joker',
+      c: 'Mr Freeze',
+      d: 'Catwoman',
+      answer: 'a'
+    },
+    {
+      question: 'What was the name of the aforementioned son?', 
+      a: 'Tim', 
+      b: 'Damian',
+      c: 'Lucious',
+      d: 'Ryan Abelman',
+      answer: 'b'
+    },
+    {
+      question: 'Being Robin has a shorter lifetime than a non-chargeable role at ACN, how many different Robins have there been (in canon)?', 
+      a: '1', 
+      b: '2',
+      c: '4',
+      d: '5',
+      answer: 'd'
+    },
+    {
+      question: 'Batman kept files on how to kill every superhero. In his file, how did he say to kill Aquaman?', 
+      a: 'Not cut plastic 6packs', 
+      b: 'BatPee in a pool',
+      c: 'Turing eels against him',
+      d: 'Make him afraid of water',
+      answer: 'd'
+    },
+    {
+      question: 'Which of the following IS a real Batman villain?', 
+      a: 'The Jester King', 
+      b: 'Hush',
+      c: 'Vulture',
+      d: 'Kingpin',
+      answer: 'b'
+    },
+    {
+      question: 'Modern adaptations show Batman NEVER doing this, when he actually did it a TON in the early comics', 
+      a: 'Flying in a helicopter', 
+      b: 'Cheating at trivia',
+      c: 'Changing the Batcave temp',
+      d: 'Murdering People',
+      answer: 'd'
+    },
+    {
+      question: 'Who voiced Batman in 7 different cartoon series', 
+      a: 'Kevin Conroy', 
+      b: 'Mark Hamill',
+      c: 'Timothy Daly',
+      d: 'Adam West',
+      answer: 'a'
+    },
+    {
+      question: 'Which actor famously took much less salary in exchange for more merchandise revenue percentage and full creative control of their Batman villain?', 
+      a: 'Danny DeVito', 
+      b: 'Jack Nicholson',
+      c: 'Jim Carrey',
+      d: 'Arnold Schwarzenegger',
+      answer: 'b'
+    },
+    {
+      question: 'SUBJECTIVE ROUND: Which is the worst Batman movie of all time?', 
+      a: 'The Dark Knight', 
+      b: 'Batman Forever',
+      c: 'Batman v Superman',
+      d: 'Batman Returns',
+      answer: 'c'
+    }
+  ]
+}
