179840290a908c40202e354a537677094f0e7fd9
doctafaustus <clowd150@gmail.com>
2020-02-07 11:03:22 -0600

    Add qs john

---
+++ questions/qs-john-images.js
@@ -0,0 +1,97 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowecase LETTER of the correct answer (a, b, c, or d)
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'Which company uses this shade of pink?',
+      imageURL: 'https://www.colorhexa.com/f8a2b7.png', 
+      a: 'Taco Bell',
+      b: 'Dunkin Donuts',
+      c: 'Vineyard Vines',
+      d: 'Lyft',
+      answer: 'c'
+    },
+    {
+      question: 'Which company uses this shade of green?',
+      imageURL: 'https://www.colorhexa.com/1ed75f.png', 
+      a: 'Android',
+      b: 'Spotify',
+      c: 'Starbucks',
+      d: 'Heineken',
+      answer: 'b'
+    },
+    {
+      question: 'Which company uses this shade of yellow?',
+      imageURL: 'https://www.colorhexa.com/fffc00.png', 
+      a: 'National Geographic',
+      b: 'Sprint',
+      c: 'Ikea',
+      d: 'Snapchat',
+      answer: 'd'
+    },
+    {
+      question: 'Which company uses this shade of orange?',
+      imageURL: 'https://www.colorhexa.com/f96302.png', 
+      a: 'Home Depot',
+      b: 'Etsy',
+      c: 'Amazon',
+      d: 'Ulta',
+      answer: 'a'
+    },
+    {
+      question: 'Which company uses this shade of blue?',
+      imageURL: 'https://www.colorhexa.com/1428a0.png', 
+      a: 'Samsung',
+      b: 'Facebook',
+      c: 'Salesforce',
+      d: 'HP',
+      answer: 'a'
+    },
+    {
+      question: 'Which company uses this shade of red?',
+      imageURL: 'https://www.colorhexa.com/e1261c.png', 
+      a: 'Farmers',
+      b: 'AAA',
+      c: 'Firemans Fund',
+      d: 'State Farm',
+      answer: 'd'
+    },
+    {
+      question: 'Which company uses this shade of purple?',
+      imageURL: 'https://www.colorhexa.com/4d148c.png', 
+      a: 'FedEx',
+      b: 'Twitch',
+      c: 'Roku',
+      d: 'Yahoo',
+      answer: 'a'
+    },
+    {
+      question: 'Which company uses this shade of gray?',
+      imageURL: 'https://www.colorhexa.com/939598.png', 
+      a: 'Apple',
+      b: 'WordPress',
+      c: 'Nintendo',
+      d: 'Forbes',
+      answer: 'c'
+    },
+    {
+      question: 'Which former/current Clearhead client uses this shade of blue?',
+      imageURL: 'https://www.colorhexa.com/0095db.png', 
+      a: 'SVB',
+      b: 'Capital One',
+      c: 'AT&T',
+      d: 'Google',
+      answer: 'a'
+    },
+    {
+      question: 'Which former/current Clearhead client uses this shade of red?',
+      imageURL: 'https://www.colorhexa.com/e21836.png', 
+      a: 'CDW',
+      b: 'Wendys',
+      c: 'Reebok',
+      d: 'New Balance',
+      answer: 'd'
+    },
+  ]
+};
