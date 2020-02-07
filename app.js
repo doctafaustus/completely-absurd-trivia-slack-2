b9352230cb67563f32823b456567664c2f2352cf
William Coloe <william.coloe@accenture.com>
2020-02-07 13:24:58 -0600

    Test John images game

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.webhookURL = prodSecretURL;
 
 // Set questions
 //qs-james-thanksgiving-images.js
-app.qs = require('./questions/qs-ra-batman.js');
+app.qs = require('./questions/qs-john-images.js');
 
 gameInit(app);
