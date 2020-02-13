e82a7a3bc9869f241c7ff5203faccbb45147f84e
William Coloe <william.coloe@accenture.com>
2020-02-13 16:21:21 -0600

    Add qs taylor

---
+++ questions/qs-taylor.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'What country was not founded by freed American slaves?', 
+      a: 'Sierra Leone', 
+      b: 'Haiti',
+      c: 'Senegal',
+      d: 'Liberia',
+      answer: 'c'
+    },
+    {
+      question: 'Which of Hawaii\'s islands is known as Pineapple Island ', 
+      a: 'Molokai', 
+      b: 'Lanai ',
+      c: 'Ni\'ihau',
+      d: 'Molokini',
+      answer: 'b'
+    },
+    {
+      question: 'How many countries does Africa have?', 
+      a: '54', 
+      b: '89',
+      c: '56',
+      d: '36',
+      answer: 'a'
+    },
+    {
+      question: 'In what country to people shake their head for \'yes\' and nod their head for \'no\'?', 
+      a: 'Georgia', 
+      b: 'Syria',
+      c: 'Italy',
+      d: 'Bulgaria',
+      answer: 'd'
+    },
+    {
+      question: 'Where is the only co-pricincipality (a country run by two princes) in the world?', 
+      a: 'Monaco', 
+      b: 'Andorra',
+      c: 'Liechtenstein',
+      d: 'Wales',
+      answer: 'b'
+    },
+    {
+      question: 'What is the most linguistically diverse country, with over 800 languages spoken?', 
+      a: 'Papau New Guinea ', 
+      b: 'Tanzania',
+      c: 'Peru',
+      d: 'India',
+      answer: 'a'
+    },
+    {
+      question: 'What country spans 12 timezones', 
+      a: 'France', 
+      b: 'China',
+      c: 'United States',
+      d: 'Russia',
+      answer: 'a'
+    },
+    {
+      question: 'What is the youngest country in Europe?', 
+      a: 'Serbia', 
+      b: 'Montenegro',
+      c: 'South Sudan',
+      d: 'Kosovo',
+      answer: 'd'
+    },
+    {
+      question: 'What country has the record for the highest recorded temperature of 136.4 degrees?', 
+      a: 'Tunisia', 
+      b: 'Zambia',
+      c: 'Libya',
+      d: 'South Africa',
+      answer: 'c'
+    },
+    {
+      question: 'What year is it in North Korea? ', 
+      a: '109', 
+      b: '2020',
+      c: '2009',
+      d: '120',
+      answer: 'a'
+    }
+  ]
+};
