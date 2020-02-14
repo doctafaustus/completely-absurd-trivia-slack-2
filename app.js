6e4a284a4b6233fdfcbd38b2f5798e15e18a51e8
doctafaustus <clowd150@gmail.com>
2020-02-14 10:10:34 -0600

    Add qs autumn, Config qs Taylor

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.webhookURL = prodRealURL;
 
 // Set questions
 //qs-james-thanksgiving-images.js
-app.qs = require('./questions/qs-john-images.js');
+app.qs = require('./questions/qs-taylor.js');
 
 gameInit(app);
---
+++ questions/qs-autumn.js
@@ -0,0 +1,88 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowecase LETTER of the correct answer (a, b, c, or d)
+
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'Which of the following is a hardwood?', 
+      a: 'Pine',
+      b: 'Fir',
+      c: 'Balsa',
+      d: 'Spruce',
+      answer: 'c'
+     },
+     {
+      question: 'Which of the following is a softwood?', 
+      a: 'Birch',
+      b: 'Cedar',
+      c: 'Cherry',
+      d: 'Oak',
+      answer: 'b'
+     },
+     {
+      question: 'The hardness of wood is ranked by what scale?', 
+      a: 'Janka',
+      b: 'Avery',
+      c: 'Stumpage',
+      d: 'Kesha',
+      answer: 'a'
+     },
+     {
+      question: 'What is Woody\'s last name from Toy Story?', 
+      a: 'Wayne',
+       b: 'Cowboy',
+       c: 'Pride',
+       d: 'Harrelson',
+      answer: 'c'
+     },
+     {
+      question: 'Which hit show about wood loggers is currently airing on History Channel?', 
+      a: 'Ax Men',
+       b: 'Swamp Loggers',
+       c: 'Tree Fellers',
+       d: 'Chainsaw Jugglers',
+      answer: 'a'
+     },
+     {
+      question: 'What country does the US have a softwood lumber dispute with?', 
+      a: 'Great Britain',
+       b: 'Mexico',
+       c: 'Canada',
+       d: 'Australia',
+      answer: 'c'
+     },
+     {
+      question: 'In the musical "Into the Woods," who played the witch in the original broadway cast?', 
+      a: 'Glenn Close',
+      b: 'Liza Minnelli',
+      c: 'Barbra Streisand',
+      d: 'Bernadette Peters',
+      answer: 'd'
+     },
+     {
+      question: 'Wood ear mushrooms are also known as what?', 
+      a: 'Pig\'s Ear',
+      b: 'Black Fungus',
+      c: 'Chanterelle',
+      d: 'Matsutake',
+      answer: 'b'
+     },
+     {
+      question: 'Which WWE wrestler was known for using a 2x4 piece of wood as a prop to intimidate his opponents?', 
+      a: 'Buzz Sawyer',
+      b: 'Drill Presser',
+      c: 'Hacksaw',
+      d: 'Biscuit Joiner',
+      answer: 'c'
+     },
+     {
+      question: 'How much wood could a woodchuck chuck (if it was equivalent to digging a burrow)?', 
+      a: '20 lbs',
+      b: '75 lbs',
+      c: '115 lbs',
+      d: '700 lbs',
+      answer: 'd'
+    }
+  ]
+};
