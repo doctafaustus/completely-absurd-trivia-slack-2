5ea7361a0e7377f360a6fdb4893360a20b585590
William Coloe <william.coloe@accenture.com>
2020-02-21 13:42:52 -0600

    Config Dune

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-tyler-name-the-state.js');
+app.qs = require('./questions/qs-tyler-dune.js');
 
 gameInit(app);
