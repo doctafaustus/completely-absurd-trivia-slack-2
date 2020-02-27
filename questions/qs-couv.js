403a4e803eca0645ff7afbe65ec08b9624fb12b8
doctafaustus <clowd150@gmail.com>
2020-02-27 13:25:55 -0600

    Add qs couv

---
+++ questions/qs-couv.js
@@ -0,0 +1,99 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// Sixpence None The Richer Trivia by Couv
+​
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'The band Sixpence None The Richer gets its name from which author?',
+      imageURL: 'https://i.ibb.co/BgygC96/1.jpg', 
+      a: 'Max Lucado',
+      b: 'Frank Peretti',
+      c: 'C.S. Lewis',
+      d: 'Jesus',
+      answer: 'c'
+    },
+    {
+      question: 'The \"S\" in C.S. Lewis\' name stands for:',
+      imageURL: 'https://i.ibb.co/n1FCxpP/2.jpg', 
+      a: 'Samuel',
+      b: 'Sixpence',
+      c: 'Simon',
+      d: 'Staples',
+      answer: 'd'
+    },
+    {
+      question: 'The sixpence coin was first minted during the reign of King',
+      imageURL: 'https://i.ibb.co/1f7n4sG/3.jpg', 
+      a: 'Henry VIII',
+      b: 'Edward VI',
+      c: 'George VI',
+      d: 'Diamond',
+      answer: 'b'
+    },
+    {
+      question: 'The song \"There She Goes\" was originally written and recorded by',
+      imageURL: 'https://i.ibb.co/5nMLQSt/4.jpg', 
+      a: 'The La\'s',
+      b: 'Robbie Williams',
+      c: 'Weird Al Yankovic',
+      d: 'The Boo Radleys',
+      answer: 'a'
+    },
+    {
+      question: 'In what year did \"Kiss Me\" hit #1 of Billboard\'s Top 40?',
+      imageURL: 'https://i.ibb.co/Cwfz4My/5.jpg', 
+      a: '1996',
+      b: '1997',
+      c: '1998',
+      d: '1999',
+      answer: 'd'
+    },
+    {
+      question: 'Who has NOT sung a cover of the Sixpence song \"Kiss Me\"?',
+      imageURL: 'https://i.ibb.co/n1zZNVW/6.jpg', 
+      a: 'Avril Lavigne',
+      b: 'SZA',
+      c: 'RG',
+      d: 'Katy Perry',
+      answer: 'd'
+    },
+    {
+      question: 'How many calories are in a single Hershey\'s Kiss?',
+      imageURL: 'https://i.ibb.co/bXP3pJR/7.jpg', 
+      a: '8',
+      b: '26',
+      c: '42',
+      d: '69',
+      answer: 'b'
+    },
+    {
+      question: 'Which song was not covered by Sixpence None The Richer?',
+      imageURL: 'https://i.ibb.co/7YGKQRm/8.jpg', 
+      a: '_Don\'t Dream It\'s Over_ by Crowded House',
+      b: '_Love Is Blindness_ by U2',
+      c: '_We Are The Champions_ by Queen',
+      d: '_Dancing Queen_ by ABBA',
+      answer: 'c'
+    },
+    {
+      question: 'In 1970, how many sixpence coins were there in \£1?',
+      imageURL: 'https://i.ibb.co/ygyGc2F/9.jpg', 
+      a: '0',
+      b: '6',
+      c: '16',
+      d: '40',
+      answer: 'd'
+    },
+    {
+      question: 'How many different band members has Sixpence None The Richer had?',
+      imageURL: 'https://i.ibb.co/7W5Txy2/10.jpg', 
+      a: '7',
+      b: '14',
+      c: '19',
+      d: '21',
+      answer: 'b'
+    }
+  ]
+};
