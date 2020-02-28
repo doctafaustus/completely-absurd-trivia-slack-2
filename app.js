49ec96bee1e632631e81eade467e5cbab98e7bcf
William Coloe <william.coloe@accenture.com>
2020-02-28 13:39:32 -0600

    Config qs Lauren

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-couv.js');
+app.qs = require('./questions/qs-lauren-pug.js');
 
 
 gameInit(app);
