0a350723448ff1040cd7468d32d7b6f19cbcdd52
William Coloe <william.coloe@accenture.com>
2020-03-05 15:13:51 -0600

    QA qs darice

---
+++ questions/qa-darice.js
@@ -0,0 +1,98 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowecase LETTER of the correct answer (a, b, c, or d)
+​
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'How much is this man squid hat?',
+      imageURL: 'https://www.dropbox.com/s/ifedrh9f1pxvta3/mansquid.png', 
+      a: '$5',
+      b: '$15',
+      c: '$30',
+      d: '$32',
+      answer: 'a'
+    },
+    {
+      question: 'How much is this Nicholas Cage pillow case? Insert NOT included.',
+      imageURL: 'https://www.dropbox.com/s/wrl8gsgcsh8m1it/pillow.png', 
+      a: '$5.25',
+      b: '$12.50',
+      c: '$18.99',
+      d: '$25.99',
+      answer: 'c'
+    },
+    {
+      question: 'How much are these Finger Covers for cheesy food?',
+      imageURL: 'https://www.dropbox.com/s/y7y3r00n2ajfr7h/fingercovers.png', 
+      a: '$1.45',
+      b: '$5.29',
+      c: '$12.29',
+      d: '$15.75',
+      answer: 'c'
+    },
+    {
+      question: 'This Andy Warhol sketch was purchased for $5 at a garage sale. How much is it worth?',
+      imageURL: 'https://www.dropbox.com/s/01l3m1zf2oj44s9/andywarhol.png', 
+      a: '$75,000',
+      b: '$400,000',
+      c: '$1.2 Million',
+      d: '$2 Million',
+      answer: 'd'
+    },
+    {
+      question: 'How much did a typical house cost in 1915?',
+      imageURL: 'https://www.dropbox.com/s/hhgrv91s8cjiewx/house.png', 
+      a: '$1,500',
+      b: '$3,200',
+      c: '$5,000',
+      d: '$10,000',
+      answer: 'b'
+    },
+    {
+      question: 'In God We Rust 2005 Kansas State Quarter',
+      imageURL: 'https://www.dropbox.com/s/d7ltf8posscbo2t/coin.png', 
+      a: '10',
+      b: '100',
+      c: '250',
+      d: '500',
+      answer: 'b'
+    },
+    {
+      question: 'How much is a Kobe Beef and Main Lobster Burger in Burger Brasserie Las Vegas?',
+      imageURL: 'https://www.dropbox.com/s/1hcweywwfe0ure4/burger.png', 
+      a: '$182',
+      b: '$235',
+      c: '$777',
+      d: '$1590',
+      answer: 'c'
+    },
+    {
+      question: 'What breed was the world\'s most expensive dog selling at $2 million?',
+      imageURL: 'https://www.dropbox.com/s/j7nitxh5n06jhot/pawprint.png', 
+      a: 'Golden Doodle',
+      b: 'Shikoku',
+      c: 'Siberian Husky',
+      d: 'Tibetan Mastiff',
+      answer: 'd'
+    },
+    {
+      question: 'Pirates of the Caribbean - On Stranger Tides holds the record for most expensive production. How much was it?',
+      imageURL: 'https://www.dropbox.com/s/cteenwwmgw07f2x/movie.png', 
+      a: '$378.5 million',
+      b: '$415.2 million',
+      c: '$543.1 million',
+      d: '$625 million',
+      answer: 'a'
+    },
+    {
+      question: 'What is the average household income in America?',
+      imageURL: 'https://www.dropbox.com/s/3ivsmcl87yxr2gt/money.png', 
+      a: '$42,572',
+      b: '$52,139',
+      c: '$61,937',
+      d: '$62,312',
+      answer: 'c'
+    }
+  ]
+};
