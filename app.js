d44605f7945ae37a9745a64c7cc6af64769d72b1
William Coloe <william.coloe@accenture.com>
2020-03-13 14:06:25 -0500

    Config qs James

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-lauren-2.js');
+app.qs = require('./questions/qs-james-virus.js');
 
 
 gameInit(app);
