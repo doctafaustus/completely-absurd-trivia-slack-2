6f38ee2e911057a912459b0c36fd90888a5430f7
doctafaustus <clowd150@gmail.com>
2020-03-27 16:32:15 -0500

    Config who said it

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-ssc.js');
+app.qs = require('./questions/qs-who-said-it.js');
 
 
 gameInit(app);
