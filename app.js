af0bd585279b7c13fec13762b45be595bd0a7a12
doctafaustus <clowd150@gmail.com>
2020-03-27 16:38:24 -0500

    Config qs adam

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-who-said-it.js');
+app.qs = require('./questions/qs-adam-1.js');
 
 
 gameInit(app);
