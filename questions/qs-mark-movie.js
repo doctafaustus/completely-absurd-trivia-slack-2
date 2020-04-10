83d5726cc5c29d51eadd4b3166d9d216e6837f15
doctafaustus <clowd150@gmail.com>
2020-04-10 11:48:13 -0500

    Stash mark game

---
+++ questions/qs-mark-movie.js
@@ -0,0 +1,95 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://i.ibb.co/k6btm72/7378-10931-1.jpg', 
+      a: 'eight men out',
+      b: 'a league of their own',
+      c: 'the rookie',
+      d: 'major league',
+      answer: 'b'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://i.ibb.co/mvtzCvR/maxresdefault-1.jpg', 
+      a: 'rookie of the year',
+      b: 'bull durham',
+      c: 'the natural',
+      d: 'angels in the outfield',
+      answer: 'a'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://i.ibb.co/mD4LjBv/PSH-Moneyball-593x356.jpg', 
+      a: 'major league',
+      b: 'the sandlot',
+      c: 'bull durham',
+      d: 'moneyball',
+      answer: 'd'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://i.ibb.co/rH7nd6g/seriously-1.jpg', 
+      a: 'major league',
+      b: 'major league two',
+      c: 'major league: back to the minors',
+      d: 'none of the above',
+      answer: 'b'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://i.ibb.co/GvJ3CZz/major-league-minor-rev-1.jpg', 
+      a: 'major league',
+      b: 'major league two',
+      c: 'major league: back to the minors',
+      d: 'none of the above',
+      answer: 'c'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://d2j1wkp1bavyfs.cloudfront.net/wp-content/uploads/2019/04/Major-League-1989-sports-movies-23262040-1280-720.jpg', 
+      a: 'major league',
+      b: 'major league two',
+      c: 'major league: back to the minors',
+      d: 'none of the above',
+      answer: 'a'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://m.media-amazon.com/images/M/MV5BOWY2YWFkZWItZmI3NS00NDIxLTkwYzEtYjdlZGY0YTJjYThhXkEyXkFqcGdeQXVyNTkyODcwNjc@._V1_.jpg', 
+      a: 'bull durham',
+      b: 'the natural',
+      c: 'mr baseball',
+      d: 'the rookie',
+      answer: 'c'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://resizing.flixster.com/vCKVI4weUnOXyOEQQsMa1WEI9ME=/300x300/v1.aDsyNjU4O2o7MTgzNjA7MTIwMDs3MDA7NDU2', 
+      a: 'the sandlot',
+      b: 'the benchwarmers',
+      c: 'fever pitch',
+      d: 'summer catch',
+      answer: 'b'
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://www.themoviescene.co.uk/reviews/_img/2873-2.jpg', 
+      a: 'mr 3000',
+      b: 'mr baseball',
+      c: 'major league 2',
+      d: 'field of dreams',
+      answer: ''
+    },
+    {
+      question: 'which movie was it?',
+      imageURL: 'https://m.media-amazon.com/images/M/MV5BOWUwYjVjMDYtMmUzOC00ZTNlLTkwMTYtODJiZGFjNTA5ZjlmXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg', 
+      a: 'bingo long',
+      b: 'field of dreams',
+      c: 'the sandlot',
+      d: 'the sandlot 2',
+      answer: 'a'
+    },
+  ]
+};
