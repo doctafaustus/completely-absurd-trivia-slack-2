ef718b88483c50a49915c8f5d2a934a053b08438
William Coloe <william.coloe@accenture.com>
2020-04-16 16:22:33 -0500

    Add qs james netflix

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-gillian.js');
+app.qs = require('./questions/qs-adam-1.js');
 
 
 gameInit(app);
---
+++ questions/qs-james-netflix.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'In what year was Netflix founded?', 
+      a: '2002', 
+      b: '1997',
+      c: '1999',
+      d: '2004',
+      answer: 'b'
+    },
+    {
+      question: 'Who offered to buy-out Netflix in the year 2000?', 
+      a: 'Barnes and Noble', 
+      b: 'Sam Goody',
+      c: 'Blockbuster',
+      d: 'Accenture',
+      answer: 'c'
+    },
+    {
+      question: 'Who founded Netflix?', 
+      a: 'A couple of white dudes bc Silicon Valley', 
+      b: 'Robot Overlords',
+      c: 'Fred P. Accenture',
+      d: 'Matty Wishnow',
+      answer: 'a'
+    },
+    {
+      question: 'What was Netflix\'s annual revenue for 2019?', 
+      a: '$13.4 billion', 
+      b: '$15 billion',
+      c: '$18.2 billion',
+      d: '$20.2 billion',
+      answer: 'd'
+    },
+    {
+      question: 'In what year did Netflix begin offering streaming content?', 
+      a: '2007', 
+      b: '2008',
+      c: '2009',
+      d: '2010',
+      answer: 'a'
+    },
+    {
+      question: 'What was the 1st film distributed by Netflix?', 
+      a: 'The Ridiculous 6', 
+      b: 'Pee-wee\'s Big Holiday',
+      c: 'Beasts of No Nation',
+      d: 'The Do-Over',
+      answer: 'c'
+    },
+    {
+      question: 'What was the first program (show) distributed by Netflix', 
+      a: 'Hemlock Grove', 
+      b: 'House of Cards',
+      c: 'Orange is the New Black',
+      d: 'Bloodline',
+      answer: 'b'
+    },
+    {
+      question: 'By the end of 2019, how many world-wide users did Netflix have?', 
+      a: '167 million', 
+      b: '140 million',
+      c: '200 million',
+      d: '102 million',
+      answer: 'a'
+    },
+    {
+      question: 'What Netflix film was the first ever to be nominated for Best Picture?', 
+      a: 'The Irishman', 
+      b: 'Marriage Story',
+      c: 'Pee-wee\'s Big Holiday',
+      d: 'Roma',
+      answer: 'd'
+    },
+    {
+      question: 'When will The Office (US) no longer be available on Netflix?', 
+      a: 'Aug 2020', 
+      b: 'Now',
+      c: 'Nov 2022',
+      d: 'Jan 2021',
+      answer: 'd'
+    }
+  ]
+};
