2a861d90f83d95a8a506a58c1ca78fea5a400d63
doctafaustus <clowd150@gmail.com>
2020-04-16 18:04:23 -0500

    Add and config qs andrew

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-adam-1.js');
+app.qs = require('./questions/qs-andrew.js');
 
 
 gameInit(app);
---
+++ questions/qs-andrew.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'The movie "Amadeus" is about which famous composer?', 
+      a: 'Beethoven', 
+      b: 'Mozart',
+      c: 'Bach',
+      d: 'Handel',
+      answer: 'b'
+    },
+    {
+      question: 'Opera originated in which country?', 
+      a: 'France', 
+      b: 'Germany',
+      c: 'England',
+      d: 'Italy',
+      answer: 'd'
+    },
+    {
+      question: "You'll hear this piece each 4th of July with fireworks and canon shots:", 
+      a: "Beethoven's 9th Symph", 
+      b: 'Flight of the Bumblebee',
+      c: '1812 Overture',
+      d: 'Canon in D Major',
+      answer: 'c'
+    },
+    {
+      question: 'Who is arguably THE greatest operatic tenor of the modern era?', 
+      a: 'Luciano Pavarotti', 
+      b: 'Placido Domingo',
+      c: 'Andrea Bocelli',
+      d: 'Josh Groban',
+      answer: 'a'
+    },
+    {
+      question: 'Philip Glass composes in what musical style?', 
+      a: 'Romantic', 
+      b: 'Minimalism',
+      c: 'Baroque',
+      d: 'Jazz',
+      answer: 'b'
+    },
+    {
+      question: 'Which type of trumpet has NO valves (buttons to press)?', 
+      a: 'B-flat', 
+      b: 'Piccolo',
+      c: 'Tooter',
+      d: 'Bugle',
+      answer: 'd'
+    },
+    {
+      question: "An orchestra's Concert Master holds what position?", 
+      a: 'Conductor', 
+      b: '1st Violin',
+      c: '1st Flute',
+      d: '1st Horn',
+      answer: 'b'
+    },
+    {
+      question: 'How many keys on the modern piano?', 
+      a: '104', 
+      b: '88',
+      c: '56',
+      d: '206',
+      answer: 'b'
+    },
+    {
+      question: 'Which one is NOT a percussion instrument?', 
+      a: 'Snare', 
+      b: 'Marimba',
+      c: 'Double Bass',
+      d: 'Triangle',
+      answer: 'c'
+    },
+    {
+      question: 'One of the most important jazz saxophonists of all time:', 
+      a: 'Miles Davis', 
+      b: 'Billie Holiday',
+      c: 'John Coltrane',
+      d: 'Maynard Ferguson',
+      answer: 'c'
+    }
+  ]
+};
