50b7a71221f076985e67145d811f096231b6980a
doctafaustus <clowd150@gmail.com>
2020-04-17 13:11:56 -0500

    QA game

---
+++ questions/qs-elizabeth.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'What Is the airspeed velocity of an unladen swallow?',
+      a: 'Top Gun',
+      b: 'Monty Python and the Holy Grail',
+      c: 'Die Hard',
+      d: 'Karate Kid',
+      answer: 'b'
+    },
+    {
+      question: 'For the LAST TIME we did not order a giant trampoline?',
+      a: 'Bill and Ted’s Excellent Adventure',
+      b: 'Aladdin',
+      c: 'The Emperor\'s New Groove',
+      d: 'Chicken Little',
+      answer: 'c'
+    },
+    {
+      question: 'Gee, I\'m really sorry your mom blew up, Ricky... guess she won\'t be able to eat any spicy foods for awhile.',
+      a: 'Ghostbusters',
+      b: 'Bottle Rocket',
+      c: 'The Cannonball Run',
+      d: 'Better off Dead',
+      answer: 'd'
+    },
+    {
+      question: 'Never go in against a Sicilian when DEATH is on the line!',
+      a: 'Gladiator',
+      b: 'Helen of Troy',
+      c: '300',
+      d: 'Princess Bride',
+      answer: 'd'
+    },
+    {
+      question: 'Go to your house?? In 3D land?',
+      a: 'Space Jam',
+      b: 'Mary Poppins',
+      c: 'The SpongeBob SquarePants Movie',
+      d: 'Enchanted',
+      answer: 'a'
+    },
+    {
+      question: 'I don’t have a bandit mask but I modified this tube sock',
+      a: 'Moonrise Kingdom',
+      b: 'The Pink Panther',
+      c: 'Fantastic Mr. Fox',
+      d: 'Meet the Robinsons',
+      answer: 'c'
+    },
+    {
+      question: 'You love Ladysmith Black Mambazo!',
+      a: 'Princess Diaries ',
+      b: 'Ms. Congeniality',
+      c: 'The Lizzie Mcguire Movie',
+      d: 'Mean Girls',
+      answer: 'd'
+    },
+    {
+      question: 'The secret ingredient is rattlesnake venom. Really makes the color pop. It’s perfectly harmless… when it’s dry.',
+      a: 'My Name is Nobody',
+      b: 'Holes',
+      c: 'The Hateful Eight',
+      d: 'Hell or Highwater',
+      answer: 'b'
+    },
+    {
+      question: 'Does it come in black?',
+      a: 'Men in Black',
+      b: 'Batman Begins',
+      c: 'The Terminator',
+      d: 'Mr. & Mrs. Smith',
+      answer: 'b'
+    },
+    {
+      question: 'You’re an embarrassment to nature, you know that?',
+      a: 'How to Train your Dragon',
+      b: 'Brother Bear',
+      c: 'Finding Nemo',
+      d: 'Ice Age',
+      answer: 'd'
+    }
+  ]
+};
