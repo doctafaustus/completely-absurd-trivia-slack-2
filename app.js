655e9f52c363367fb281b4c607e6cb6be876188a
doctafaustus <clowd150@gmail.com>
2020-04-17 13:27:59 -0500

    Ready up for gametime

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ app.get('/', (req, res) => {
 const prodSecretURL = process.env.PORT ? process.env.prodSecretURL : fs.readFileSync(`${__dirname}/private/webhook.txt`).toString();
 const catv2URL = process.env.catv2URL
 const prodRealURL = process.env.prodRealURL;
-app.webhookURL = prodSecretURL;
+app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-elizabeth.js');
+app.qs = require('./questions/qs-andrew.js');
 
 
 gameInit(app);
