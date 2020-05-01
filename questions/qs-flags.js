9704e25df58c1c91094965b4d0d3619c1fc1b159
William Coloe <william.coloe@accenture.com>
2020-04-30 23:12:05 -0500

    Add qs flags and sleep

---
+++ questions/qs-flags.js
@@ -0,0 +1,96 @@
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/X4gP51T/south-korea-flag-icon-256-1.png', 
+      a: 'Philippines',
+      b: 'Thailand',
+      c: 'India',
+      d: 'South Korea',
+      answer: 'd'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/ZLBg418/39d0bacaf3-450-1.png', 
+      a: 'Poland',
+      b: 'Austria',
+      c: 'Germany',
+      d: 'Slovakia',
+      answer: 'a'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/HHj9HB1/3595c7d426-450.png', 
+      a: 'Sudan',
+      b: 'Chad',
+      c: 'Kenya',
+      d: 'South Africa',
+      answer: 'c'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/Bqb3hFK/NZ-flag-1.png', 
+      a: 'Australia',
+      b: 'New Zealand',
+      c: 'Puerto Rico',
+      d: 'Wales',
+      answer: 'b'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/K2KPDqZ/5eab05d0bf-450-1.png', 
+      a: 'Egypt',
+      b: 'Saudi Arabia',
+      c: 'Israel',
+      d: 'Lebanon',
+      answer: 'd'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/K71nppp/finland-1.png', 
+      a: 'Denmark',
+      b: 'Sweden',
+      c: 'Norway',
+      d: 'Finland',
+      answer: 'd'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/DkbTZBx/f33723b85d-450-1.png', 
+      a: 'Afghanistan',
+      b: 'Pakistan',
+      c: 'Turkey',
+      d: 'Yemen',
+      answer: 'a'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/dJk9x0R/d9d5260102-450-1.png', 
+      a: 'Greenland',
+      b: 'Panama',
+      c: 'Iceland',
+      d: 'Chile',
+      answer: 'b'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/SKpxR8n/4e754d5d5b-450-1.png', 
+      a: 'Armenia',
+      b: 'Barbados',
+      c: 'Georgia',
+      d: 'Bhutan',
+      answer: 'c'
+    },
+    {
+      question: 'Which country\'s flag is this?',
+      imageURL: 'https://i.ibb.co/tcsSPjY/f79f878290-450-1.png', 
+      a: 'Croatia',
+      b: 'Czech Republic',
+      c: 'Switzerland',
+      d: 'Trinidad & Tobago',
+      answer: 'd'
+    }
+  ]
+};
+
---
+++ questions/qs-josh.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'On average, how many hours do squirrels spend sleeping every day?', 
+      a: '8 hours', 
+      b: '3 hours',
+      c: '14 hours',
+      d: '10 hours',
+      answer: 'c'
+    },
+    {
+      question: 'Which of the animals below is NOT able to sleep while standing?', 
+      a: 'Elephants', 
+      b: 'Flamingos',
+      c: 'Horses',
+      d: 'White-Tailed Deer',
+      answer: 'd'
+    },
+    {
+      question: 'Which animal regularly spends upwards of 20-22 hours a day sleeping?', 
+      a: 'Sloth', 
+      b: 'North American Opossum',
+      c: 'Koala',
+      d: 'Giant Armadillo',
+      answer: 'c'
+    },
+    {
+      question: 'After giving birth, these dedicated mothers will go an average of 3 weeks without ANY sleep.', 
+      a: 'Human', 
+      b: 'Bottlenose Dolphin',
+      c: 'Peregrine Falcon',
+      d: 'African Leopard',
+      answer: 'b'
+    },
+    {
+      question: 'Sleeping an average of just 30 minutes a day, these animals have one of the shortest sleep requirements in the entire animal kingdom.', 
+      a: 'Giraffe', 
+      b: 'Pygmy Shrew',
+      c: 'Sheep',
+      d: 'Ring-Tailed Lemur',
+      answer: 'a'
+    },
+    {
+      question: 'Unihemispheric Slow-Wave Sleep (USWS) describes a state where some animals are able to:', 
+      a: 'sleep with both eyes open', 
+      b: 'greatly slow their heart rate',
+      c: 'rest one half of the brain',
+      d: 'teleport to Narnia',
+      answer: 'c'
+    },
+    {
+      question: 'Which of these animals is NOT considered a "True Hibernator"?', 
+      a: 'Brown Bat', 
+      b: 'Ground Squirrel',
+      c: 'Woodchuck',
+      d: 'Grizzly Bear',
+      answer: 'd'
+    },
+    {
+      question: 'What triggered humans to move from a biphasic sleep schedule (two periods of sleep), to monophasic?', 
+      a: 'Industrialization', 
+      b: 'Invention of the alarm clock',
+      c: 'Street lighting',
+      d: 'Lack of natural predators',
+      answer: 'a'
+    },
+    {
+      question: 'For humans over the age of 18, what is the minimum recommended hours of sleep per night?', 
+      a: '6', 
+      b: '6.5',
+      c: '7',
+      d: '8',
+      answer: 'c'
+    },
+    {
+      question: 'What is the human record for the longest period without sleep?', 
+      a: '15 days', 
+      b: '6 days',
+      c: '11 days',
+      d: '9 days',
+      answer: 'c'
+    }
+  ]
+};
