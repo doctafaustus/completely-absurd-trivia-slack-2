bc5e442a98d201722ddace516fcbed2b4028dff2
doctafaustus <clowd150@gmail.com>
2020-05-15 09:57:05 -0500

    QA qs Darice 2

---
+++ questions/qs-darice2.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'Who once got on a flight while going into anaphylactic shock and didn\'t realize it until after they landed?', 
+      a: 'Mercan', 
+      b: 'Gillian',
+      c: 'JoCo',
+      d: 'Ed',
+      answer: 'b'
+    },
+    {
+      question: 'Who lost 3 toenails during a half marathon?', 
+      a: 'Jlowe', 
+      b: 'Marion',
+      c: 'Crystal',
+      d: 'Marcus',
+      answer: 'c'
+    },
+    {
+      question: 'Who is related to the first female full-time assistant coach in NBA history - Becky Hammon?', 
+      a: 'Caitlin', 
+      b: 'Elizadeath',
+      c: 'Sarah S.',
+      d: 'Beth D.',
+      answer: 'd'
+    },
+    {
+      question: 'Who had their appendix removed at 12, got a staph infection in the incision, then had the lead role in Oliver Twist two weeks later?', 
+      a: 'Josh N.', 
+      b: 'Mitch',
+      c: 'Marcus',
+      d: 'Juan',
+      answer: 'a'
+    },
+    {
+      question: 'Who used to volunteer at a children\'s shelter?', 
+      a: 'Melissa', 
+      b: 'Molly',
+      c: 'Jane',
+      d: 'Sean',
+      answer: 'c'
+    },
+    {
+      question: 'Whose partner still gets royalty checks for steamy romance novels they had written?', 
+      a: 'Aaron', 
+      b: 'Jeremy',
+      c: 'RA',
+      d: 'David N.',
+      answer: 'a'
+    },
+    {
+      question: 'Who used to take the balls out of computer mice and roll them across the room in school?', 
+      a: 'Andrew', 
+      b: 'Billy',
+      c: 'Brady',
+      d: 'Phillip',
+      answer: 'b'
+    },
+    {
+      question: 'Who is an Eagle Scout and earned more than 30 merit badges?', 
+      a: 'Geoff', 
+      b: 'David N.',
+      c: 'Nick R.',
+      d: 'Kevin',
+      answer: 'c'
+    },
+    {
+      question: 'Who once casually perused a sock rack with Aziz Ansari?  When asked "You\'re Aziz Ansari, right?" he replied back, "Heh, yeah dude" in a way that only Aziz can.', 
+      a: 'Tay', 
+      b: 'Darice',
+      c: 'Colby',
+      d: 'Adam',
+      answer: 'c'
+    },
+    {
+      question: 'Past Clearheader Bonus! Who made their parents buy them a big CEO desk in 5th grade?', 
+      a: 'Brendan', 
+      b: 'Abby',
+      c: 'Jake',
+      d: 'Dante',
+      answer: 'd'
+    }
+  ]
+};
