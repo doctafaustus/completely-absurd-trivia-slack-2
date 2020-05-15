e05f8279eef06103d66977149a6709f9adcdc38f
doctafaustus <clowd150@gmail.com>
2020-05-15 09:49:51 -0500

    QA qs john2

---
+++ questions/qs-john2.js
@@ -0,0 +1,100 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Images must be under .5mb
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// All games must be 10 questions - do not delete any!
+
+module.exports = {
+  gameType: 'question',
+  questions: [
+    {
+      question: 'Which of the following is NOT one of Rileys five emotions in Inside Out?',
+      imageURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2019-06/11/12/enhanced/buzzfeed-prod-web-05/enhanced-32257-1560271454-13.jpg', 
+      a: 'Disgust',
+      b: 'Anxiety',
+      c: 'Anger',
+      d: 'Joy',
+      answer: 'b'
+    },
+    {
+      question: 'What kind of fish is Dory?',
+      imageURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2019-06/11/12/enhanced/buzzfeed-prod-web-05/enhanced-32604-1560271641-1.jpg', 
+      a: 'Blue saddleback',
+      b: 'Blue damselfish',
+      c: 'Blue cichlid',
+      d: 'Blue tang',
+      answer: 'd'
+    },
+    {
+      question: 'What kind of dessert does Kevin the bird from Up love?',
+      imageURL: 'https://secure.i.telegraph.co.uk/multimedia/archive/01500/kevin_1500262i.jpg', 
+      a: 'Chocolate',
+      b: 'Marshmallows',
+      c: 'Skittles',
+      d: 'Pretzels filled with peanut butter',
+      answer: 'a'
+    },
+    {
+      question: 'What is Boos nickname for Sully in Monsters, Inc.?',
+      imageURL: 'https://i.pinimg.com/originals/8c/0c/65/8c0c657b22a8ee33c450db0e36968465.jpg', 
+      a: 'Fluffy',
+      b: 'Doggy',
+      c: 'Kitty',
+      d: 'Bear',
+      answer: 'c'
+    },
+    {
+      question: 'What is the musical that WALL-E watches repeatedly?',
+      imageURL: 'https://happycouchpanda.files.wordpress.com/2016/04/wall-e-watching-tv-web.jpg', 
+      a: 'Hello, Dolly!',
+      b: 'Showtune',
+      c: 'Mack and Mabel',
+      d: 'Dear World',
+      answer: 'a'
+    },
+    {
+      question: 'What kind of dog is Danté?',
+      imageURL: 'https://i0.wp.com/growingupbilingual.com/wp-content/uploads/2017/08/image005-1.jpg', 
+      a: 'Azawakh',
+      b: 'Borzoi',
+      c: 'Puli',
+      d: 'Xoloitzcuintli',
+      answer: 'd'
+    },
+    {
+      question: 'What is the name of the kid on the tricycle who catches Mr. Incredible lifting a car over his head?',
+      imageURL: 'https://i.ytimg.com/vi/LTX51OzGndw/hqdefault.jpg', 
+      a: 'Billy',
+      b: 'Buddy',
+      c: 'Rusty',
+      d: 'Mikey',
+      answer: 'c'
+    },
+    {
+      question: 'What is the screenname of the toy that Trixie is instant messaging on Bonnies laptop?',
+      imageURL: 'https://vignette.wikia.nocookie.net/pixar/images/9/94/Rexandtrixie.png', 
+      a: 'PteriDACTOR872',
+      b: 'VelociSTAR237',
+      c: 'TrikeTERROR984',
+      d: 'RexATRONA113',
+      answer: 'b'
+    },
+    {
+      question: 'What is the name of the toy cleaner who fixes Woody in Toy Story 2?',
+      imageURL: 'https://i.pinimg.com/originals/20/9a/8f/209a8fd3f299796282cd2bd47b79c077.jpg', 
+      a: 'Harold',
+      b: 'Carl',
+      c: 'Geri',
+      d: 'Robert',
+      answer: 'c'
+    },
+    {
+      question: 'Which is NOT one of the excuses that Horst gives in Ratatouille for why he was sent to prison?',
+      imageURL: 'https://y.yarn.co/37b24a3b-49c6-4df1-a826-df9798b56f16_screenshot.jpg', 
+      a: 'Blackmail',
+      b: 'Fraud',
+      c: 'Making a hole in the ozone',
+      d: 'Bank robbery',
+      answer: 'a'
+    },
+  ]
+};
