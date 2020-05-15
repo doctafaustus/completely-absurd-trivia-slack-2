c297f8213e9eed76770c5514fd3b01f042e6470e
doctafaustus <clowd150@gmail.com>
2020-05-15 13:36:45 -0500

    Config qs darice2

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-john2.js');
+app.qs = require('./questions/qs-darice2.js');
 
 
 gameInit(app);
