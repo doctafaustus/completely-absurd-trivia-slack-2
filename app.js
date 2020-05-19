52a6902ea2cb3b5b82e1ae11d842d08ef074c7ef
doctafaustus <clowd150@gmail.com>
2020-05-19 14:59:07 -0500

    QA qs phillip

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodSecretURL;
 
 // Set questions
-app.qs = require('./questions/qs-mark-text3.js');
+app.qs = require('./questions/qs-phillip.js');
 
 
 gameInit(app);
