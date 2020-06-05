36aded9ef53688e8fe534249eac34cc9336272fa
doctafaustus <clowd150@gmail.com>
2020-06-05 10:14:54 -0500

    Config qs 5

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-phillip.js');
+app.qs = require('./questions/qs-5.js');
 
 
 gameInit(app);
---
+++ questions/qs-5.js
@@ -0,0 +1,89 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// All games must be 10 questions - do not delete any!
+
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: '"Tool Time" was a show within what other show?', 
+      a: 'This Olde House', 
+      b: 'This Old House',
+      c: 'All That',
+      d: 'Home Improvement',
+      answer: 'd'
+    },
+    {
+      question: 'Where is the Sea of Tranquility located?', 
+      a: 'Spain', 
+      b: 'Saudi Arabia',
+      c: 'Israel',
+      d: 'The Moon',
+      answer: 'd'
+    },
+    {
+      question: 'What is the most common letter in the English alphabet?', 
+      a: 'E', 
+      b: 'S',
+      c: 'A',
+      d: 'I',
+      answer: 'a'
+    },
+    {
+      question: 'What does a Scoville unit measure?', 
+      a: 'Molecular mass', 
+      b: 'Spiciness',
+      c: 'Computer mouse movement',
+      d: 'How fast a beard grows',
+      answer: 'b'
+    },
+    {
+      question: 'What company makes the Butterfinger bar?', 
+      a: 'Mars', 
+      b: 'Nestle',
+      c: 'Hershey',
+      d: 'Lindt',
+      answer: 'b'
+    },
+    {
+      question: 'What does "NC" mean in an NC-17 movie rating?', 
+      a: 'Nudity-Crudeness', 
+      b: 'Nude Content',
+      c: 'No Children',
+      d: 'Not Compliant',
+      answer: 'c'
+    },
+    {
+      question: 'Where did rap superstar Eminem grow up?', 
+      a: 'Chicago', 
+      b: 'Detroit',
+      c: 'NYC',
+      d: 'Oklahoma',
+      answer: 'b'
+    },
+    {
+      question: 'Which actor was NOT a regular on "Whose Line Is it Anyway"?', 
+      a: 'Ricky Gervais', 
+      b: 'Ryan Stiles',
+      c: 'Colin Mochrie',
+      d: 'Wayne Brady',
+      answer: 'a'
+    },
+    {
+      question: 'What is the main ingredient in a falafel?', 
+      a: 'Chickpea', 
+      b: 'Flour',
+      c: 'Pita',
+      d: 'Bulgur Wheat',
+      answer: 'a'
+    },
+    {
+      question: 'Where is the uvula located?', 
+      a: 'Next to the Cerebellum', 
+      b: 'Female genitals',
+      c: 'Throat',
+      d: 'Male genitals',
+      answer: 'c'
+    }
+  ]
+};
