c3512568f397b8afc0413f5f3157050f2bd88a52
William Coloe <william.coloe@accenture.com>
2020-06-10 23:06:58 -0500

    Add qs chelsb-goodbye

---
+++ questions/qs-chels-goodbye.js
@@ -0,0 +1,88 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// All games must be 10 questions - do not delete any!
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'What is the first custom emoji that chelsB made?', 
+      a: ':asana:', 
+      b: ':wisco:',
+      c: ':nicmoji:',
+      d: ':yeti:',
+      answer: 'b'
+    },
+    {
+      question: 'What is the first custom emoji at clearhead?', 
+      a: ':scout:', 
+      b: ':fyou:',
+      c: ':doge:',
+      d: ':party-parrot:',
+      answer: 'a'
+    },
+    {
+      question: 'Who made the first custom emoji at clearhead?', 
+      a: 'Brendan', 
+      b: 'Perica',
+      c: 'Haley',
+      d: 'Sophie',
+      answer: 'c'
+    },
+    {
+      question: 'How many custom emojis does clearhead have?', 
+      a: '1339', 
+      b: '921',
+      c: '1816',
+      d: '2198',
+      answer: 'c'
+    },
+    {
+      question: 'Of those 1816, what percentage did chelsB make?', 
+      a: '10%', 
+      b: '15%',
+      c: '20%',
+      d: '25%',
+      answer: 'd'
+    },
+    {
+      question: 'Who made the :mothership-lasers: emoji?', 
+      a: 'Nick B', 
+      b: 'West',
+      c: 'John',
+      d: 'easy-e',
+      answer: 'b'
+    },
+    {
+      question: 'Who made the :siren: emoji?', 
+      a: 'Couv', 
+      b: 'Haley',
+      c: 'Marion',
+      d: 'Perica',
+      answer: 'a'
+    },
+    {
+      question: 'Who made the :maisey-means-business: emoji?', 
+      a: 'Molly', 
+      b: 'Devon',
+      c: 'Libby',
+      d: 'Jalaliani',
+      answer: 'd'
+    },
+    {
+      question: 'Who made the :party_dead: emoji?', 
+      a: 'Jenni', 
+      b: 'Joco',
+      c: 'Autumn',
+      d: 'Billy',
+      answer: 'a'
+    },
+    {
+      question: 'What company will chelsB miss the most?', 
+      a: ':classic-accenture:', 
+      b: ':clearhead:',
+      c: ':svb:',
+      d: ':mcmichael-lowe-mcmichael:',
+      answer: 'b'
+    }
+  ]
+}
