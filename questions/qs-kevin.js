9b16342e8139c4f6ad9578e9bc7405c66d01e47d
William Coloe <william.coloe@accenture.com>
2020-06-10 22:16:47 -0500

    Add qs Kevin

---
+++ questions/qs-kevin.js
@@ -0,0 +1,89 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// All games must be 10 questions - do not delete any!
+
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'What TV show did Kevin work on?', 
+      a: 'Law & Order SVU', 
+      b: 'Breaking Bad',
+      c: 'Monk',
+      d: 'Enlightened',
+      answer: 'c'
+    },
+    {
+      question: 'Where did Kevin and Marion meet?', 
+      a: 'At Marion\'s work HH in NY', 
+      b: 'Working at CH',
+      c: 'On christianmingle.com',
+      d: 'At Kev\'s roommate\'s bday',
+      answer: 'a'
+    },
+    {
+      question: 'What town in NJ is Kevin from?', 
+      a: 'Morristown', 
+      b: 'Montclair',
+      c: 'Newbridge',
+      d: 'Rutherford',
+      answer: 'd'
+    },
+    {
+      question: 'What sport did Kevin play extensively as a child?', 
+      a: 'Soccer', 
+      b: 'Football',
+      c: 'Volleyball',
+      d: 'Baseball',
+      answer: 'd'
+    },
+    {
+      question: 'In Kevin\'s opinion, what\'s the best slasher movie?', 
+      a: 'Halloween', 
+      b: 'Happy Death Day',
+      c: 'The Unseen',
+      d: 'Scream',
+      answer: 'a'
+    },
+    {
+      question: 'What phrase did Kev coin during a Shins concert in Coney Island in the late 2000\'s?', 
+      a: 'All the things!', 
+      b: 'Sports!',
+      c: 'Fail!',
+      d: 'Cool Story Bro!',
+      answer: 'b'
+    },
+    {
+      question: 'Which of these was NOT one of Kevin\'s CHILDHOOD pets?', 
+      a: 'Lucy', 
+      b: 'Bogsy',
+      c: 'Quirky',
+      d: 'Bailey',
+      answer: 'a'
+    },
+    {
+      question: 'Kevin\'s great grandparents are from what country?', 
+      a: 'Romania', 
+      b: 'Greece',
+      c: 'Armenia',
+      d: 'Italy',
+      answer: 'c'
+    },
+    {
+      question: 'What is Kevin\'s favorite sports team of all time?', 
+      a: 'Boston Red Sox', 
+      b: 'New York Yankees',
+      c: 'New York Mets',
+      d: 'Los Angeles Dodgers',
+      answer: 'b'
+    },
+    {
+      question: 'Which celebrity is sort of Kevin\'s cousin?', 
+      a: 'Keira Knightley', 
+      b: 'Natalie Portman',
+      c: 'Alison Brie',
+      d: 'Rooney Mara',
+      answer: 'd'
+    }
+  ]
+};
