46d2bb9ce27afeb0ee1d1d0047851257699bc0c6
doctafaustus <clowd150@gmail.com>
2020-06-12 13:07:01 -0500

    Config chelsB game

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-kevin.js');
+app.qs = require('./questions/qs-chels-goodbye.js');
 
 
 gameInit(app);
