ff8b35b0a2cd27fe3212976ab73bf1b21a54a2fb
doctafaustus <clowd150@gmail.com>
2020-06-19 13:33:02 -0500

    Config qs lotr

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-juneteenth.js');
+app.qs = require('./questions/qs-lotr.js');
 
 
 gameInit(app);
