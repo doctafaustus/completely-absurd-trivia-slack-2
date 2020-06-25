6a3ff4469a59b89fc00ec0f94294f236c6091af8
doctafaustus <clowd150@gmail.com>
2020-06-25 09:13:32 -0500

    QA cara 1

---
+++ questions/qs-cara-1.js
@@ -0,0 +1,89 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// All games must be 10 questions - do not delete any!
+
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'How many countries in the world still have a monarchy?', 
+      a: '40', 
+      b: '27',
+      c: '43',
+      d: '39',
+      answer: 'c'
+    },
+    {
+      question: 'In the Princess Bride, what animal do they fight in the forest?', 
+      a: 'Skunk', 
+      b: 'Cat',
+      c: 'Weasel',
+      d: 'Rat',
+      answer: 'd'
+    },
+    {
+      question: 'Which Disney Princess was the first princess to appear in a feature-length film?', 
+      a: 'Snow White', 
+      b: 'Cinderella',
+      c: 'Persephone',
+      d: 'Pochahontas',
+      answer: 'a'
+    },
+    {
+      question: 'Which Princess a solar power company that provides street lights, reading lamps, and electricity to 620 million people in Africa?', 
+      a: 'Keisha Omilana of Nigeria', 
+      b: 'Abze Djigma of Burkina Faso',
+      c: 'Sikhanyiso of Swaziland',
+      d: 'Sylvia Nagginda of Buganda',
+      answer: 'b'
+    },
+    {
+      question: 'The inspiration for Ariel from Little Mermaid’s look was based on which actress’ face?', 
+      a: 'Shannon Doherty', 
+      b: 'Marcia Cross',
+      c: 'Alyssa Milano',
+      d: 'Holly Marie Combs',
+      answer: 'c'
+    },
+    {
+      question: 'What city did Princess Diana die in? #RIP', 
+      a: 'Montreal, Canada', 
+      b: 'London, England',
+      c: 'Rome, Italy',
+      d: 'Paris, France',
+      answer: 'd'
+    },
+    {
+      question: 'In the esteemed 2001 film, Princess Diaries, where is the country of Genovia located?', 
+      a: 'Between France and  Spain', 
+      b: 'Next to Switzerland',
+      c: 'Next to Russia',
+      d: 'Between France and Italy',
+      answer: 'a'
+    },
+    {
+      question: 'Before she became Queen of England, Elizabeth joined the army as a:', 
+      a: 'Nurse', 
+      b: 'Mechanic',
+      c: 'Chief Commander',
+      d: 'Radar Operator',
+      answer: 'b'
+    },
+    {
+      question: 'The only princess ever to win an Oscar is:', 
+      a: 'Audrey Hepburn', 
+      b: 'Meghan Markle',
+      c: 'Grace Kelly ',
+      d: 'Tilda Swinton',
+      answer: 'c'
+    },
+    {
+      question: 'Jane nicknamed me Princess in response to whose relentless and unfounded criticism?', 
+      a: 'D-Rap', 
+      b: 'Jlowe',
+      c: 'Joco',
+      d: 'RA',
+      answer: 'b'
+    }
+  ]
+};
---
+++ questions/qs-programmer.js
@@ -0,0 +1,89 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// All games must be 10 questions - do not delete any!
+
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'The JS Console is is both an information log and a ___', 
+      a: 'Automation Pipeline', 
+      b: 'REPL',
+      c: 'Task Runner',
+      d: 'Bundler',
+      answer: 'b'
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    },
+    {
+      question: '', 
+      a: '', 
+      b: '',
+      c: '',
+      d: '',
+      answer: ''
+    }
+  ]
+};
