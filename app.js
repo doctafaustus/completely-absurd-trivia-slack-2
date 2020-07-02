cbc9d2662839987529e31c4b01999d2300b9c615
doctafaustus <clowd150@gmail.com>
2020-07-02 13:26:40 -0500

    QA mitch game

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
 // Webhook URLs
 const prodSecretURL = process.env.PORT ? process.env.prodSecretURL : fs.readFileSync(`${__dirname}/private/webhook.txt`).toString();
 const prodRealURL = process.env.prodRealURL;
-app.webhookURL = prodRealURL;
+app.webhookURL = prodSecretURL;
 
 // Set questions
 app.qs = require('./questions/qs-mitch-farewell-1.js');
