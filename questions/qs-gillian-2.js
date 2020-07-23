d2b0dd7684f107c70ab9a911d1d2e7ea078a60c5
William Coloe <william.coloe@accenture.com>
2020-07-23 18:24:50 -0500

    QA qs gillian 2

---
+++ questions/qs-gillian-2.js
@@ -0,0 +1,85 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'The chicken genome was sequenced in what year making it the first bird sequenced.',
+      a: '2002',
+      b: '2005',
+      c: '2008',
+      d: '1999',
+      answer: 'b'
+    },
+    {
+      question: 'Which bird family gave us the domesticated chicken?',
+      a: 'Pheasants',
+      b: 'Grouse',
+      c: 'Warblers',
+      d: 'Thrushes',
+      answer: 'a'
+    },
+    {
+      question: 'An elephant never forgets anything! What do chickens never forget?',
+      a: 'A smell',
+      b: 'A sound',
+      c: 'A face',
+      d: 'Another chicken',
+      answer: 'c'
+    },
+    {
+      question: 'How many eyelids to chickens have?',
+      a: '4',
+      b: '3',
+      c: '2',
+      d: '1.5',
+      answer: 'b'
+    },
+    {
+      question: 'Humans are up to 60% water. What percent water is a chicken?',
+      a: '40%',
+      b: '50%',
+      c: '61',
+      d: '75',
+      answer: 'd'
+    },
+    {
+      question: 'Run, Chicken, Run! How fast can poultry pound the pavement?',
+      a: '3 mph',
+      b: '9 mph',
+      c: '11mph',
+      d: 'Chickens only fly',
+      answer: 'b'
+    },
+    {
+      question: 'Which of the following is a part of a rooster mating dance?',
+      a: 'The Cock-a-doodle-doo',
+      b: 'A backflip',
+      c: 'A moonwalk',
+      d: 'A wing drag',
+      answer: 'd'
+    },
+    {
+      question: 'We all know a chicken can live with its head cut off. Ew. How long did the longest make it?',
+      a: '18 minutes',
+      b: '18 days',
+      c: '18 weeks',
+      d: '18 months',
+      answer: 'd'
+    },
+    {
+      question: 'What mineral do chickens actively avoid eating?',
+      a: 'sodium',
+      b: 'potassium',
+      c: 'iodine',
+      d: 'calcium',
+      answer: 'a'
+    },
+    {
+      question: 'How many chickens are there on the planet currently?',
+      a: '2 billion',
+      b: '10 billion',
+      c: '25 billion',
+      d: '35 billion',
+      answer: 'c'
+    }
+  ]
+};
