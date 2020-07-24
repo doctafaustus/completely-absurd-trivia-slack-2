5c52bccd88727985c2112cc4337d796f2fb8cec0
doctafaustus <clowd150@gmail.com>
2020-07-24 13:35:39 -0500

    Config web dev triv

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-gillian-2.js');
+app.qs = require('./questions/qs-6.js');
 
 
 gameInit(app);
---
+++ questions/qs-aaron.js
@@ -0,0 +1,84 @@
+module.exports = {
+  gameType: 'text',
+  questions: [
+    {
+      question: 'What is the name of Angela Lansbury titular detective on Murder, She Wrote?', 
+      a: 'Jessica Parker', 
+      b: 'Jessica Fletcher',
+      c: 'Jessica Fontenot',
+      d: 'Jessica Palmer',
+      answer: 'b'
+    },
+    {
+      question: 'Before her role as Jessica Fletcher, Dame Lansbury played this detective.', 
+      a: 'Miss Marple', 
+      b: 'Miss Maisel',
+      c: 'Miss Maple',
+      d: 'Miss Marvel',
+      answer: 'a'
+    },
+    {
+      question: 'What is Jessica Fletcher\'s hometown?', 
+      a: 'Portland', 
+      b: 'Cabot Cove',
+      c: 'Boston',
+      d: 'Newburry',
+      answer: 'b'
+    },
+    {
+      question: 'Sheriff Amos Tupper was played by this actor for 19 episodes?', 
+      a: 'Tom Selleck', 
+      b: 'Tom Bosley ',
+      c: 'Tom Hanlon',
+      d: 'Tommy Lee Jones',
+      answer: 'b'
+    },
+    {
+      question: 'Murder, She Wrote season 3 had a crossover with this show.', 
+      a: 'Cagney and Lacey', 
+      b: 'Magnum P.I.',
+      c: 'Miami Vice',
+      d: 'Law and Order',
+      answer: 'b'
+    },
+    {
+      question: 'How old was Angela Lansbury when the show premiered?', 
+      a: '48', 
+      b: '58',
+      c: '68',
+      d: '78',
+      answer: 'b'
+    },
+    {
+      question: 'In the TV show Jessica never...', 
+      a: 'drove a car.', 
+      b: 'rode a bike.',
+      c: 'got drunk.',
+      d: 'fired a gun.',
+      answer: 'a'
+    },
+    {
+      question: 'Murder she wrote offical mechandise includes ', 
+      a: 'a board game.', 
+      b: 'a CD of orginal music sung by Lansbury as Fletcher.',
+      c: 'a set trading cards.',
+      d: 'a set of pinochle cards.',
+      answer: 'a'
+    },
+    {
+      question: 'Murder, She Wrote produced one spin off TV show.', 
+      a: 'Remington Steele', 
+      b: 'Kojak',
+      c: 'The Law & Harry McGraw',
+      d: 'Murder, He wrote',
+      answer: 'c'
+    },
+    {
+      question: 'According to the BBC, the Cabot Cove had about 1,490 murders per million people every year or a rate of...', 
+      a: '5.3 murders every year', 
+      b: '10.3 murders every year',
+      c: '12.3 murders every year',
+      d: '15.3 murders every year',
+      answer: 'a'
+    }
+  ]
