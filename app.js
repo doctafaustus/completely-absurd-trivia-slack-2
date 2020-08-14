a40987ee265605f6f90d18865a2c03ed2ccc7008
doctafaustus <clowd150@gmail.com>
2020-08-14 11:40:25 -0500

    Config qs Brady

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
 // Webhook URLs
 const prodSecretURL = process.env.PORT ? process.env.prodSecretURL : fs.readFileSync(`${__dirname}/private/webhook.txt`).toString();
 const prodRealURL = process.env.prodRealURL;
-app.webhookURL = prodSecretURL;
+app.webhookURL = prodRealURL;
 
 // Set questions
 app.qs = require('./questions/qs-brady.js');
