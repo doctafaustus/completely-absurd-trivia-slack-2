994ae45b1ea05be7c2c203f9f8d2eedfe95af4a2
doctafaustus <clowd150@gmail.com>
2020-08-14 13:37:05 -0500

    Config qs quarantine triv

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-brady.js');
+app.qs = require('./questions/qs-quarantine-hobbies-2.js');
 
 
 gameInit(app);
