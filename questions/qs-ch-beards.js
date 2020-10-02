85802ce4ed36ec6c10e4a669f7dff92a9eb1c109
doctafaustus <clowd150@gmail.com>
2020-10-02 15:41:31 -0500

    Add beard triv pics

---
+++ questions/qs-ch-beards.js
@@ -0,0 +1,111 @@
+// Answer text must be under 25 characters otherwise they'll be truncated
+// Images must be under .5mb
+// Answer value but be the lowercase LETTER of the correct answer (a, b, c, or d)
+// All games must be 10 questions - do not delete any!
+
+module.exports = {
+  gameType: 'question',
+  specialCategory: 'beards',
+  questions: [
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/6ZdjRrV/1-Beard-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/56bxVnn/1-Keanu-Reeves-1.png'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/nb8wZYQ/2-Beard-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/GC8nph6/2-Idris-Elba-1.png'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/fX5cHWb/3-Beard-small-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/594VHXF/3-Paul-Rudd-small-1.jpg'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/ZVV49Xq/4-Beard-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/Nyppf9D/4-Clooney-small-1.png'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/nwXggfH/5-Beard-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/3p9YRvr/5-Rickey-Martin-1.png'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/xGNNxvK/6-Beard-small-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/xGNNxvK/6-Beard-small-1.png'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/JFnC7X7/7-Beard-small-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/qYqQP6z/7-Jason-Momoa-small.jpg'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/M8sFxJq/8-Beard-1.jpg', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/LJxvrnV/8-Donald-Glover-1.png'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/3FKHNTr/9-Beard-1.png', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/ZBtLNxY/9-Dev-Petel-1.png'
+    },
+    {
+      question: 'Whose beard is this?',
+      imageURL: 'https://i.ibb.co/wRtmQ7Z/10-Beard-small-1.jpg', 
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      answer: '',
+      beardRevealImg: 'https://i.ibb.co/VMHyV79/10-Michael-Fassbender-small-1.jpg'
+    },
+  ]
+};
