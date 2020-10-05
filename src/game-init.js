00a0892a374b8d7432deaa7e2ebb25f13db74a2b
doctafaustus <clowd150@gmail.com>
2020-10-05 16:50:59 -0500

    Test slash commands

--- src/game-init.js
+++ src/game-init.js
@@ -83,7 +83,7 @@ module.exports = function gameInit(app) {
 
 
   // Broadcast
-  app.post('/broadcast', app.urlencodedParser, checkAdmin, (req, res) => {
+  app.post('/slack/events/broadcast', app.urlencodedParser, checkAdmin, (req, res) => {
     console.log('/broadcast');
     res.status(200).end();
     
