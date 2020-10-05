7d86689590e3fd7515aa5eee4ffb7482c09e3be2
doctafaustus <clowd150@gmail.com>
2020-10-05 18:21:51 -0500

    QA beards game

--- src/core-components/next-question.js
+++ src/core-components/next-question.js
@@ -20,7 +20,7 @@ module.exports = function nextQuestion(app) {
     const correctAnswerText = evaluateAnswers(app);
 
     // If beard game then show beard image
-    if (app.qs.questions.specialCategory === 'bears') {
+    if (app.qs.specialCategory === 'bears') {
       console.log('showing beard result');
       sendMessageToSlack(app.webhookURL, app.qs.questions[app.game.currentQuestion].beardRevealImg);
     }
