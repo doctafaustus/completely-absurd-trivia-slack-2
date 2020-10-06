21181eb41b573897dc39642a3c4d85adaba97a5a
doctafaustus <clowd150@gmail.com>
2020-10-05 19:32:17 -0500

    QA qs beards

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.webhookURL = prodSecretURL;
 console.log('prodSecretURL', prodSecretURL); 
 
 // Set questions
-app.qs = require('./questions/qs-ch-movies.js');
+app.qs = require('./questions/qs-ch-beards.js');
 
 
 gameInit(app);
