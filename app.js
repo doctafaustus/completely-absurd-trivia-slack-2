52af39775354c1eefe7284aff3f4585ecf375762
doctafaustus <clowd150@gmail.com>
2020-10-09 13:44:20 -0500

    Add qs bees

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-ch-movies.js');
+app.qs = require('./questions/qs-ch-bees.js');
 
 
 gameInit(app);
